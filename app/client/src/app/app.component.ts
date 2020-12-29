a11f88131608b9f39b2850ed5416f38930989d56
Joshua McMichael <josh@jmcmichael.com>
2020-12-29 09:55:40 -0600

    set up absolute path referencing

--- app/client/src/app/app.component.ts
+++ app/client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// import { environment as dev } from '../environments/environment';
+// import { environment as prod } from '../environments/environment.prod';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isCollapsed = false;
+
+
 }
