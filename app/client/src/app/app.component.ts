8bb6ea1958128bbb8431bfc1abebbf0074f67642
Joshua McMichael <josh@jmcmichael.com>
2020-12-29 10:07:00 -0600

    set up @app alias for absolute imports

--- app/client/src/app/app.component.ts
+++ app/client/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 
-// import { environment as dev } from '../environments/environment';
-// import { environment as prod } from '../environments/environment.prod';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
+
 export class AppComponent {
   isCollapsed = false;
-
 
 }
