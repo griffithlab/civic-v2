9cb42c2e1f0d63f97c18e9f4fab3b405689732e6
Joshua McMichael <josh@jmcmichael.com>
2021-02-12 15:15:07 -0600

    fixed '[element] is not a known element' testing errors

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -11,6 +12,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     }).compileComponents();
   });
 
