4e2b2b9a2af206ad696e3d33c6398b9f792e65ff
Joshua McMichael <josh@jmcmichael.com>
2021-02-18 14:17:14 -0600

    got route nav test working with async/await

--- client/src/app/app-routing.module.spec.ts
+++ client/src/app/app-routing.module.spec.ts
@@ -1,42 +1,33 @@
 import { Location } from '@angular/common';
-import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
+import { AppComponent } from "./app.component";
 
 import { routes } from './app-routing.module';
 
 describe('Router: App', () => {
   let location: Location;
   let router: Router;
+  let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule.withRoutes(routes)],
-      declarations: []
+      declarations: [AppComponent]
     });
 
     router = TestBed.inject(Router);
     location = TestBed.inject(Location);
+    fixture = TestBed.createComponent(AppComponent);
 
     router.initialNavigation();
   });
 
-  it('fakeAsync works', fakeAsync(() => {
-    let promise = new Promise(resolve => {
-      setTimeout(resolve, 10);
-    });
-    let done = false;
-    promise.then(() => (done = true));
-    tick(50);
-    expect(done).toBeTruthy();
-  }));
-
-  it('navigate to "" redirects you to /welcome', fakeAsync(() => {
-    router.navigate(['']).then(() => {
-      expect(location.path()).toBe('/welcome');
-    });
-    tick(50);
-  }));
+  it('navigate to "" redirects to /welcome', async () => {
+    await router.navigate(['']);
+    expect(location.path()).toBe('/welcome');
+  });
 
   // it('navigate to "search" takes you to /search', fakeAsync(() => {
   //   router.navigate(["/search"]).then(() => {
