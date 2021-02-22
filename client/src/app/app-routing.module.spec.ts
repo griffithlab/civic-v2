bfc352b232956e284ca9aa08f0cd2f2598fe0ab1
Josh McMichael <josh@jmcmichael.com>
2021-02-22 09:00:04 -0600

    added tests for primary entity list/serach views

--- client/src/app/app-routing.module.spec.ts
+++ client/src/app/app-routing.module.spec.ts
@@ -24,14 +24,43 @@ describe('Router: App', () => {
     router.initialNavigation();
   });
 
-  it('navigate to "" redirects to /welcome', async () => {
+  it('navigating to "genes" directs browser to /genes', async () => {
+    await router.navigate(['genes']);
+    expect(location.path()).toBe('/genes');
+  });
+
+  it('navigating to "assertions" directs browser to /assertions', async () => {
+    await router.navigate(['assertions']);
+    expect(location.path()).toBe('/assertions');
+  });
+
+  it('navigating to "diseases" directs browser to /diseases', async () => {
+    await router.navigate(['diseases']);
+    expect(location.path()).toBe('/diseases');
+  });
+
+  it('navigating to "sources" directs browser to /sources', async () => {
+    await router.navigate(['sources']);
+    expect(location.path()).toBe('/sources');
+  });
+
+  it('navigating to "variants" directs browser to /variants', async () => {
+    await router.navigate(['sources']);
+    expect(location.path()).toBe('/sources');
+  });
+
+  it('navigating to "variant-groups" directs browser to /variant-groups', async () => {
+    await router.navigate(['variant-groups']);
+    expect(location.path()).toBe('/variant-groups');
+  });
+
+  /*
+   * REDIRECTS
+   */
+
+  it('navigating to "" redirects to /welcome', async () => {
     await router.navigate(['']);
     expect(location.path()).toBe('/welcome');
   });
 
-  // it('navigate to "search" takes you to /search', fakeAsync(() => {
-  //   router.navigate(["/search"]).then(() => {
-  //     expect(location.path()).toBe("/search");
-  //   });
-  // }));
 });
