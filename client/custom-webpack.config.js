9fd91ddddeea5fba503015da167a019f1dc33367
Josh McMichael <josh@jmcmichael.com>
2021-02-22 16:56:02 -0600

    updated webpack config according to ng-zorro docs

--- client/custom-webpack.config.js
+++ client/custom-webpack.config.js
@@ -3,19 +3,16 @@
 * configured within angular.json's builder params and within customWebpackConfig options.
 * See https://www.npmjs.com/package/@angular-builders/custom-webpack for documentation.
 */
-"use strict";
-
-const webpack = require("webpack");
+'use strict';
 
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 
 module.exports = {
-  // WARNING: MUST set the 'mode' manually because it isn't done by NX/NG cli
   mode,
   module: {
     rules: [
       {
-        test: /\.less$/,
+        test   : /\.less$/,
         loader: 'less-loader',
         options: {
           // modifyVars: { // modify theme variable
@@ -25,10 +22,38 @@ module.exports = {
           // },
           javascriptEnabled: true
         }
-      },
-    ],
-  },
-  plugins: [
-    // add custom plugins here
-  ],
+      }
+    ]
+  }
 };
+
+
+// "use strict";
+
+// const webpack = require("webpack");
+
+// const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+
+// module.exports = {
+//   // WARNING: MUST set the 'mode' manually because it isn't done by NX/NG cli
+//   mode,
+//   module: {
+//     rules: [
+//       {
+//         test: /\.less$/,
+//         loader: 'less-loader',
+//         options: {
+//           // modifyVars: { // modify theme variable
+//           //   'primary-color': '#1DA57A',
+//           //   'link-color': '#1DA57A',
+//           //   'border-radius-base': '2px'
+//           // },
+//           javascriptEnabled: true
+//         }
+//       },
+//     ],
+//   },
+//   plugins: [
+//     // add custom plugins here
+//   ],
+// };
