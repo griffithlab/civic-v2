186efb240c02dda2ababc9ed2ca261aac8c7bef6
Josh McMichael <josh@jmcmichael.com>
2021-02-22 09:03:41 -0600

    organized app-routing module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,18 +2,27 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
+  // Main Entity Pages
   {
+    path: 'genes',
+    loadChildren: () => import('./pages/genes/genes.module')
+      .then(m => m.GenesModule)
+  },
+
+  // Misc. App Views
+  {
+    path: 'welcome',
+    loadChildren: () => import('./pages/welcome/welcome.module')
+      .then(m => m.WelcomeModule),
+  },
+
+  // Redirects
+  { // if no route provided, load welcome view
     path: '',
     pathMatch: 'full',
     redirectTo: '/welcome'
-  },
-  {
-    path: 'welcome',
-    loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule),
-
-    // path: 'genes',
-    // loadChildren: () => import('./pages/genes/genes.module').then(m => m.GenesModule),
-  }];
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
