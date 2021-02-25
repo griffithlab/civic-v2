e421f928b73a80d8f93c136033aabc4fc22cffdc
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-02-25 09:37:44 -0600

    Add new graphql query endpoint for a single gene

--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -7,5 +7,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
     field :browseGenes, resolver: Resolvers::BrowseGenes
+    field :gene, Types::Entities::GeneType, null: true do
+      description "Find a gene by CIViC ID"
+      argument :id, ID, required: true
+    end
+
+    def gene(id: )
+      Gene.includes(:sources, :variants, :gene_aliases).find(id)
+    end
   end
 end
