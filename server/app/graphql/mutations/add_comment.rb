c15908f52e80bb91d57a506289381a15ba6a1707
Adam Coffman <acoffman@wustl.edu>
2021-02-25 12:44:16 -0600

    use top level errors instead of data errors

--- server/app/graphql/mutations/add_comment.rb
+++ server/app/graphql/mutations/add_comment.rb
@@ -5,13 +5,12 @@ class Mutations::AddComment < Mutations::BaseMutation
   argument :organization_id, Int, required: false
 
   field :comment, Types::Entities::CommentType, null: true
-  field :errors, [Types::Errors::UserError], null: true
 
   def authorized?(organization_id: nil, **kwargs)
     if context[:current_user].nil?
-      return false, { errors: [ { message: 'You must be logged in to perform that action.' } ] }
+      raise GraphQL::ExecutionError, 'You must be logged in to perform that action.'
     elsif ! PolicyHelpers.can_act_for_org?( user: context[:current_user], organization_id: organization_id)
-      return false, { errors: [ { message: "User cannot comment on behalf of #{organization_id}" } ] }
+      raise GraphQL::ExecutionError, "User cannot comment on behalf of organization ##{organization_id}" 
     else
       return true
     end
@@ -29,19 +28,12 @@ class Mutations::AddComment < Mutations::BaseMutation
       if res.succeeded?
         {
           comment: res.comment,
-          errors: nil
         }
       else
-        {
-          comment: nil,
-          errors: res.errors.map { |e| { message: e } }
-        }
+        raise GraphQL::ExecutionError, res.errors.join(', ')
       end
     else
-      {
-        comment: nil,
-        errors: [{message: 'Subject not found'}]
-      }
+      raise GraphQL::ExecutionError, 'Subject not found.'
     end
   end
 end
--- server/app/graphql/types/errors/user_error.rb
+++
@@ -1,8 +0,0 @@
-module Types::Errors
-  class UserError < Types::BaseObject
-    description 'An error in user provided data'
-
-    field :message, String, null: true
-    field :path, [String], null: true, description: 'Input causing the error'
-  end
-end
