44119d1f176d347d699e553372c5ded4dd628084
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-02-25 09:30:14 -0600

    Fix variant type graphql type definition

--- server/app/graphql/types/entities/variant_type.rb
+++ server/app/graphql/types/entities/variant_type.rb
@@ -1,9 +1,9 @@
-module Types:Entities
+module Types::Entities
   class VariantType < Types::BaseObject
     field :id, Int, null: false
     field :name, String, null: false
     field :description, String, null: false
     field :gene, [Types::Entities::GeneType], null: false
-    field :evidence_items, [Types::Entities::EvidenceType], null: true
+    field :evidence_items, [Types::Entities::EvidenceItemType], null: true
   end
 end
