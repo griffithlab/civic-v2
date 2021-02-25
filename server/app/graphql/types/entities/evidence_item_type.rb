f1d7b6f9a3ba38130a9072a925346343982884ef
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-02-25 10:44:46 -0600

    Fix some one-to-one field definitions

--- server/app/graphql/types/entities/evidence_item_type.rb
+++ server/app/graphql/types/entities/evidence_item_type.rb
@@ -2,6 +2,6 @@ module Types::Entities
   class EvidenceItemType < Types::BaseObject
     field :id, Int, null: false
     field :description, String, null: false
-    field :variant, [Types::Entities::VariantType], null: false
+    field :variant, Types::Entities::VariantType, null: false
   end
 end
--- server/app/graphql/types/entities/variant_type.rb
+++ server/app/graphql/types/entities/variant_type.rb
@@ -3,7 +3,7 @@ module Types::Entities
     field :id, Int, null: false
     field :name, String, null: false
     field :description, String, null: false
-    field :gene, [Types::Entities::GeneType], null: false
+    field :gene, Types::Entities::GeneType, null: false
     field :evidence_items, [Types::Entities::EvidenceItemType], null: true
   end
 end
