8bb72b2fa675718e5bc97013f26347e1d321591c
Josh McMichael <josh@jmcmichael.com>
2021-03-05 14:41:34 -0600

    browse gene view improved with page-header component

--- client/src/app/pages/genes/genes.module.ts
+++ client/src/app/pages/genes/genes.module.ts
@@ -1,24 +1,38 @@
-import { NgModule } from '@angular/core';
+// framework imports
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+// zorro imports
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
-import { IconsProviderModule } from '@app/icons-provider.module';
-
-import { GenesRoutingModule } from './genes-routing.module';
-import { GenesComponent } from './genes.component';
+// civic imports
 import { GenesBrowseComponent } from './genes-browse/genes-browse.component';
+import { GenesComponent } from './genes.component';
 import { GenesDetailComponent } from './genes-detail/genes-detail.component';
+import { GenesRoutingModule } from './genes-routing.module';
+import { IconsProviderModule } from '@app/icons-provider.module';
 
 @NgModule({
   declarations: [GenesComponent, GenesBrowseComponent, GenesDetailComponent],
   imports: [
-    NzGridModule,
-    NzTableModule,
-    IconsProviderModule,
     CommonModule,
-    GenesRoutingModule
+    GenesRoutingModule,
+    IconsProviderModule,
+    NzAvatarModule,
+    NzBreadCrumbModule,
+    NzDropDownModule,
+    NzGridModule,
+    NzPageHeaderModule,
+    NzTableModule,
+    NzTagModule,
+    NzToolTipModule,
   ]
 })
 
