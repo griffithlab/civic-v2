19f1e1736a83c7645314fcf546be5479b5b64374
Josh McMichael <josh@jmcmichael.com>
2021-03-08 10:37:59 -0600

    preliminary MyGeneInfo component using tabs

--- client/src/app/pages/genes/genes.module.ts
+++ client/src/app/pages/genes/genes.module.ts
@@ -5,10 +5,12 @@ import { NgModule } from '@angular/core';
 // zorro imports
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { NzTagModule } from 'ng-zorro-antd/tag';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
@@ -27,10 +29,12 @@ import { IconsProviderModule } from '@app/icons-provider.module';
     IconsProviderModule,
     NzAvatarModule,
     NzBreadCrumbModule,
+    NzCardModule,
     NzDropDownModule,
     NzGridModule,
     NzPageHeaderModule,
     NzTableModule,
+    NzTabsModule,
     NzTagModule,
     NzToolTipModule,
   ]
