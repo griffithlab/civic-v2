c15c056d27ea4c201301275d2de2afcb37ef045a
Josh McMichael <josh@jmcmichael.com>
2021-03-08 11:32:14 -0600

    formatting

--- client/src/app/pages/genes/genes-browse/genes-browse.component.ts
+++ client/src/app/pages/genes/genes-browse/genes-browse.component.ts
@@ -1,35 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { pluck} from 'rxjs/operators';
-import { NGXLogger } from "ngx-logger";
+import { pluck } from 'rxjs/operators';
+import { NGXLogger } from 'ngx-logger';
 
 import { GenesBrowseService } from './genes.browse.service';
-import { Gene,
-         GenesSortColumns,
-         SortDirection,
-         PageInfo
-       } from '@app/generated/civic.apollo';
+import {
+  Gene,
+  GenesSortColumns,
+  SortDirection,
+  PageInfo,
+} from '@app/generated/civic.apollo';
 
 @Component({
   selector: 'genes-browse',
   templateUrl: './genes-browse.component.html',
-  styleUrls: ['./genes-browse.component.less']
+  styleUrls: ['./genes-browse.component.less'],
 })
 export class GenesBrowseComponent implements OnInit {
   pageInfo$: Observable<any>;
   genes$: Observable<any>;
 
-  constructor(private api: GenesBrowseService,
-              private logger: NGXLogger) {
-
+  constructor(private api: GenesBrowseService, private logger: NGXLogger) {
     const source$: Observable<any> = this.api.watchGenesBrowse();
     this.genes$ = source$.pipe(pluck('data', 'browseGenes', 'nodes'));
     this.pageInfo$ = source$.pipe(pluck('data', 'browseGenes', 'pageInfo'));
-
   }
 
   ngOnInit(): void {
-    this.logger.trace("GenesBrowseComponent initialized.");
+    this.logger.trace('GenesBrowseComponent initialized.');
   }
-
 }
