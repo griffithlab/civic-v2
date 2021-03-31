c54a7c5f9eb3cbd667cfa6006379f7d2af8ff601
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-03-31 10:32:23 -0500

    Use _ in enums, where appropriate

--- server/app/graphql/types/evidence_direction_type.rb
+++ server/app/graphql/types/evidence_direction_type.rb
@@ -1,7 +1,7 @@
 module Types
   class EvidenceDirectionType < Types::BaseEnum
     value 'SUPPORTS', value: 'Supports'
-    value 'DOESNOTSUPPORT', value: 'Does Not Support'
+    value 'DOES_NOT_SUPPORT', value: 'Does Not Support'
     value 'NA', value: 'N/A'
   end
 end
