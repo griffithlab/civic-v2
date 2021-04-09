469c8100c4ac97d78579815262560271ff9d29a7
Josh McMichael <josh@jmcmichael.com>
2021-04-09 09:23:41 -0500

    committing partial work on styling the viewer button w/ avatar img

--- client/src/app/components/layout/layout-components.module.ts
+++ client/src/app/components/layout/layout-components.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 
 import { LoginButtonComponent } from './login-button/login-button.component';
 import { AuthOptionsListComponent } from './auth-options-list/auth-options-list.component';
@@ -20,12 +22,14 @@ import { ViewerButtonComponent } from './viewer-button/viewer-button.component';
   ],
   imports: [
     CommonModule,
+    NzAvatarModule,
     NzDropDownModule,
     NzPopoverModule,
     NzButtonModule,
     NzListModule,
     NzModalModule,
     NzIconModule,
+    NzSpaceModule,
   ],
   exports: [
     LoginButtonComponent,
