84b5f8c75b944a718ab6f9f93a77d5c8737b8dad
Josh McMichael <josh@jmcmichael.com>
2021-05-03 15:05:30 -0500

    fixed flagged component input type issue (with kludge)

--- client/src/app/components/shared/flaggable/flaggable.component.ts
+++ client/src/app/components/shared/flaggable/flaggable.component.ts
@@ -15,31 +15,34 @@ export interface FlaggableSubject {
   styleUrls: ['./flaggable.component.less']
 })
 export class FlaggableComponent implements AfterViewInit, OnChanges {
-  @Input() flags!: number;
+  // TODO: figure out why null option required here for flags Input
+  // to avoid "Type 'number | null' is not assignable to type 'number'."
+  // error when provided Observable<number> returning of(flags { totalCount })
+  // in gene-details template
+  @Input() flags!: number | null;
   @Input() entity!: FlaggableSubject;
   @ContentChild(FlaggableOptionsDirective,
-                {static: false, read: ElementRef}) flagged!: ElementRef;
+                {static: false, read: ElementRef}) flaggedChild!: ElementRef;
 
-  constructor(private renderer: Renderer2) {
-  }
+  constructor(private renderer: Renderer2) { }
 
   ngOnChanges(): void {
-    this.flags === 0 ? this.removeFlagged() : this.addFlagged();
+    (this.flags && this.flags === 0) ? this.removeFlagged() : this.addFlagged();
   }
 
   ngAfterViewInit(): void {
-    this.flags === 0 ? this.removeFlagged() : this.addFlagged();
+    (this.flags && this.flags === 0) ? this.removeFlagged() : this.addFlagged();
   }
 
   addFlagged(): void {
-    if(this.flagged) {
-      this.renderer.addClass(this.flagged.nativeElement, 'flagged');
+    if(this.flaggedChild) {
+      this.renderer.addClass(this.flaggedChild.nativeElement, 'flaggedChild');
     }
   }
 
   removeFlagged(): void {
-    if (this.flagged) {
-      this.renderer.removeClass(this.flagged.nativeElement, 'flagged');
+    if (this.flaggedChild) {
+      this.renderer.removeClass(this.flaggedChild.nativeElement, 'flaggedChild');
     }
   }
 }
