6148f9663a5c8b58562e76881d38d627afe0ca62
Josh McMichael <josh@jmcmichael.com>
2021-05-07 08:05:53 -0500

    refactored org-selector-btn to use simpler API

--- client/src/app/components/shared/org-selector-btn-group/org-selector-btn-group.component.ts
+++ client/src/app/components/shared/org-selector-btn-group/org-selector-btn-group.component.ts
@@ -4,12 +4,14 @@ import {
   ContentChild,
   Output,
   EventEmitter,
+  OnDestroy,
 } from '@angular/core';
 
 import {
+  Subject,
   Observable,
 } from 'rxjs';
-import { map, pluck } from 'rxjs/operators';
+import { map, pluck, takeUntil, tap } from 'rxjs/operators';
 
 import { ViewerService, Viewer } from '@app/shared/services/viewer/viewer.service';
 import { Maybe, Organization } from '@app/generated/civic.apollo';
@@ -20,20 +22,32 @@ import { OrgSelectorBtnDirective } from './org-selector-btn.directive';
   templateUrl: './org-selector-btn-group.component.html',
   styleUrls: ['./org-selector-btn-group.component.less']
 })
-export class OrgSelectorBtnGroupComponent {
-  @Output() orgSelected = new EventEmitter<Organization>();
+export class OrgSelectorBtnGroupComponent implements OnDestroy {
+  @Input() selectedOrg!: Maybe<Organization>;
+  @Output() selectedOrgChange = new EventEmitter<Organization>();
 
   @ContentChild(OrgSelectorBtnDirective, {static: false}) button!: OrgSelectorBtnDirective
 
   organizations$: Observable<Organization[]>;
   mostRecentOrg$: Observable<Maybe<Organization>>;
 
+  mostRecentOrg: Maybe<Organization>;
+
+  private destroy$ = new Subject();
+
   constructor(private viewerService: ViewerService) {
     this.organizations$ = this.viewerService.viewer$
       .pipe(map((v: Viewer) => v.organizations));
 
     this.mostRecentOrg$ = this.viewerService.viewer$
-      .pipe(pluck('mostRecentOrg'));
+      .pipe(
+        pluck('mostRecentOrg'),
+        tap((org: Maybe<Organization>) => {
+          this.mostRecentOrg = org;
+          this.selectedOrgChange.emit(org)
+        }));
+
+    this.mostRecentOrg$.pipe(takeUntil(this.destroy$)).subscribe();
   }
 
   get disabled() {
@@ -41,6 +55,12 @@ export class OrgSelectorBtnGroupComponent {
   }
 
   selectOrg(org: any): void {
-    this.orgSelected.emit(org);
+    this.mostRecentOrg = org;
+    this.selectedOrgChange.emit(org);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
