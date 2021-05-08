e11e14313949b03ca908f1af296d72088674d7bb
Adam Coffman <acoffman@wustl.edu>
2021-05-08 18:43:14 -0500

    Add user list and field list to WithRevisions interface

--- server/app/graphql/types/interfaces/with_revisions.rb
+++ server/app/graphql/types/interfaces/with_revisions.rb
@@ -4,5 +4,25 @@ module Types::Interfaces
 
     description 'A CIViC entity that can have revisions proposed to it.'
     field :revisions, resolver: Resolvers::Revisions
+
+    field :unique_revisors, [Types::Entities::UserType], null: false,
+      description: 'List of all users that have submitted a revision to this entity.'
+
+    field :revised_field_names, [String], null: false,
+      description: 'List of all fields that have at least one revision.'
+
+    def unique_revisors
+       User.where(id:
+            Event.where(action: 'revision suggested', subject: object)
+             .select(:originating_user_id)
+       ).distinct
+
+    end
+
+    def revised_field_names
+      names = Revision.where(subject: object).distinct.pluck(:field_name)
+      names.map {|n| Types::Revisions::LinkoutData.display_name(n) }
+    end
+
   end
 end
