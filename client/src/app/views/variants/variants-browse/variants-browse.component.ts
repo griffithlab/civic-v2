c4a4c268c1ce0dd9ae3d0455fed7d203221c31ee
Adam Coffman <acoffman@wustl.edu>
2021-06-15 11:17:27 -0500

    set up routing, stub in variant browse component

---
+++ client/src/app/views/variants/variants-browse/variants-browse.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cvc-variants-browse',
+  templateUrl: './variants-browse.component.html',
+  styleUrls: ['./variants-browse.component.less']
+
+})
+export class VariantsBrowseComponent  {
+
+}
