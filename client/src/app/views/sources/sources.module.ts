1d50627a63cdc6262d9bf2b8bf404d5e6798caf6
Adam Coffman <acoffman@wustl.edu>
2021-06-24 15:14:25 -0500

    hookup frontend source browse table

--- client/src/app/views/sources/sources.module.ts
+++ client/src/app/views/sources/sources.module.ts
@@ -3,13 +3,81 @@ import { CommonModule } from '@angular/common';
 
 import { SourcesRoutingModule } from './sources-routing.module';
 import { SourcesComponent } from './sources.component';
+import { FormsModule } from '@angular/forms';
+import { SharedComponentsModule } from '@app/components/shared/shared-components.module';
+import { CvcFormsModule } from '@app/forms/forms.module';
+import { IconsProviderModule } from '@app/icons-provider.module';
+import { ReactiveComponentModule } from '@ngrx/component';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCommentModule } from 'ng-zorro-antd/comment';
+import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { TimeagoModule } from 'ngx-timeago';
+import { EvidenceModule } from '../evidence/evidence.module';
+import { VariantsRoutingModule } from '../variants/variants-routing.module';
+import { SourcesBrowseComponent } from './sources-browse/sources-browse.component';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 
 @NgModule({
-  declarations: [SourcesComponent],
+  declarations: [
+    SourcesComponent,
+    SourcesBrowseComponent
+  ],
   imports: [
     CommonModule,
-    SourcesRoutingModule
+    SourcesRoutingModule,
+    SharedComponentsModule,
+    VariantsRoutingModule,
+    IconsProviderModule,
+    NzAvatarModule,
+    NzBadgeModule,
+    NzButtonModule,
+    NzCardModule,
+    NzCommentModule,
+    NzFormModule,
+    NzDescriptionsModule,
+    NzDropDownModule,
+    NzGridModule,
+    NzListModule,
+    NzPageHeaderModule,
+    NzPopoverModule,
+    NzSpaceModule,
+    NzSpinModule,
+    NzSkeletonModule,
+    NzTableModule,
+    NzTabsModule,
+    NzTagModule,
+    NzToolTipModule,
+    NzTypographyModule,
+    ReactiveComponentModule,
+    TimeagoModule.forChild(),
+    NzInputModule,
+    FormsModule,
+    SharedComponentsModule,
+    EvidenceModule,
+    SharedComponentsModule,
+    NzSelectModule
+  ],
+  exports: [
+    SourcesBrowseComponent
   ]
 })
 export class SourcesModule { }
