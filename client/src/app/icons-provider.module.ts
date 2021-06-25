ff10d6183d14783b314410436feb085b73bd81a5
Josh McMichael <josh@jmcmichael.com>
2021-06-25 15:44:52 -0500

    getting closer to svg-to-ts generating object that can be passed directly into addIcon()

--- client/src/app/icons-provider.module.ts
+++ client/src/app/icons-provider.module.ts
@@ -8,7 +8,14 @@ import {
   DashboardOutline
 } from '@ant-design/icons-angular/icons';
 
+import  * as civicIcons from  '@app/generated/civic.icons';
+import { CivicIconLiteral } from '@app/generated/civic.icons';
+
 const icons = [MenuFoldOutline, MenuUnfoldOutline, DashboardOutline, FormOutline];
+
+// TODO: import civic.icons, create IconDefinition for each, append to icons array
+console.log(icons);
+console.log(civicIcons);
 
 @NgModule({
   imports: [NzIconModule],
@@ -18,4 +25,8 @@ const icons = [MenuFoldOutline, MenuUnfoldOutline, DashboardOutline, FormOutline
   ]
 })
 export class IconsProviderModule {
+}
+
+export function toIconDef(iconLiterals: CivicIconLiteral[]): CivicIconLiteral[] {
+  return iconLiterals;
 }
