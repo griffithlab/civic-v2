4f6416f83c31567c6e34cd84f28c748f4906963b
Josh McMichael <josh@jmcmichael.com>
2021-07-12 16:14:03 -0500

    better tag-list component & style documentation

--- client/src/app/components/shared/tag-list/tag-list.component.ts
+++ client/src/app/components/shared/tag-list/tag-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+/**
+ * Wraps a set of <cvc-[entity]-tag> elements to apply styles that ensure
+ * internal margins between tag rows, and appropriate padding with its parent
+ */
 @Component({
   selector: 'cvc-tag-list',
   templateUrl: './tag-list.component.html',
