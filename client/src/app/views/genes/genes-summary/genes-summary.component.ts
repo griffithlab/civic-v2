3d06e41981e859af0a71bd50a6942aa537496bf6
Josh McMichael <josh@jmcmichael.com>
2021-07-13 11:19:17 -0500

    geneId needs to be coerced to number after refactor to observable params

--- client/src/app/views/genes/genes-summary/genes-summary.component.ts
+++ client/src/app/views/genes/genes-summary/genes-summary.component.ts
@@ -43,7 +43,7 @@ export class GenesSummaryComponent implements OnDestroy {
       let observable = queryRef.valueChanges;
 
       this.subscribableEntity = {
-        id: params.geneId,
+        id: +params.geneId,
         entityType: SubscribableEntities.Gene,
       };
 
