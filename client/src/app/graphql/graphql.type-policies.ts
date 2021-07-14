b9ee6c98c1cc9a016cede8a0b4d11870e6e30b14
Adam Coffman <acoffman@wustl.edu>
2021-07-14 14:08:23 -0500

    add new fields to type policies, re-format

--- client/src/app/graphql/graphql.type-policies.ts
+++ client/src/app/graphql/graphql.type-policies.ts
@@ -1,8 +1,5 @@
-import { makeVar, ReactiveVar, TypePolicies } from "@apollo/client/cache";
-import { relayStylePagination } from "@apollo/client/utilities";
-import { Organization, User } from "@app/generated/civic.apollo";
-import gql from "graphql-tag";
-import { Maybe } from "@app/generated/civic.apollo";
+import { TypePolicies } from '@apollo/client/cache';
+import { relayStylePagination } from '@apollo/client/utilities';
 
 export const CvcTypePolicies: TypePolicies = {
   Gene: {
@@ -10,27 +7,91 @@ export const CvcTypePolicies: TypePolicies = {
       comments: relayStylePagination(),
       revisions: relayStylePagination(),
       events: relayStylePagination(),
-      variants: relayStylePagination()
-    }
+      variants: relayStylePagination(),
+    },
   },
   Query: {
     fields: {
-      browseGenes: relayStylePagination(["entrezSymbol", "drugName", "geneAlias", "diseaseName"]),
-      browseVariants: relayStylePagination(["variantName", "entrezSymbol", "diseaseName", "drugName"]),
-      browseVariantGroups: relayStylePagination(["name", "geneNames", "variantNames"]),
-      browseSources: relayStylePagination(["name", "year", "sourceType", "citationId", "author", "journal"]),
-      browseDiseases: relayStylePagination(["name", "doid", "geneNames"]),
-      events: relayStylePagination(["subject", "organizationId", "originatingUserId", "eventType"]),
-      variants: relayStylePagination(["geneId", "name", "evidenceStatusFilter"]),
-      comments: relayStylePagination(["originatingUserId", "subject"]),
-      evidenceItems: relayStylePagination(["diseaseName", "drugName", "id", "description", "evidenceLevel", "evidenceDirection", "clinicalSignificance", "evidenceType", "evidenceRating", "variantOrigin", "variantId", "assertionId", "organizationId", "userId"]),
-      assertions: relayStylePagination(["diseaseName", "drugName", "id", "summary", "assertionDirection", "clinicalSignificance", "assertionType", "variantId", "ampLevel", "geneName", "variantName", "evidenceId", "organizationId", "userId"]),
-      organizations: relayStylePagination(["name", "id"]),
-      flags: relayStylePagination(["flaggable", "flaggingUserId", "resolvingUserId", "state"]),
-    }
-  }
+      browseGenes: relayStylePagination([
+        'entrezSymbol',
+        'drugName',
+        'geneAlias',
+        'diseaseName',
+      ]),
+      browseVariants: relayStylePagination([
+        'variantName',
+        'entrezSymbol',
+        'diseaseName',
+        'drugName',
+      ]),
+      browseVariantGroups: relayStylePagination([
+        'name',
+        'geneNames',
+        'variantNames',
+      ]),
+      browseSources: relayStylePagination([
+        'name',
+        'year',
+        'sourceType',
+        'citationId',
+        'author',
+        'journal',
+      ]),
+      browseDiseases: relayStylePagination(['name', 'doid', 'geneNames']),
+      events: relayStylePagination([
+        'subject',
+        'organizationId',
+        'originatingUserId',
+        'eventType',
+      ]),
+      variants: relayStylePagination([
+        'geneId',
+        'name',
+        'evidenceStatusFilter',
+      ]),
+      comments: relayStylePagination(['originatingUserId', 'subject']),
+      evidenceItems: relayStylePagination([
+        'diseaseName',
+        'drugName',
+        'id',
+        'description',
+        'evidenceLevel',
+        'evidenceDirection',
+        'clinicalSignificance',
+        'evidenceType',
+        'evidenceRating',
+        'variantOrigin',
+        'variantId',
+        'assertionId',
+        'organizationId',
+        'userId',
+        'phenotypeId',
+      ]),
+      assertions: relayStylePagination([
+        'diseaseName',
+        'drugName',
+        'id',
+        'summary',
+        'assertionDirection',
+        'clinicalSignificance',
+        'assertionType',
+        'variantId',
+        'ampLevel',
+        'geneName',
+        'variantName',
+        'evidenceId',
+        'organizationId',
+        'userId',
+        'phenotypeId',
+      ]),
+      organizations: relayStylePagination(['name', 'id']),
+      flags: relayStylePagination([
+        'flaggable',
+        'flaggingUserId',
+        'resolvingUserId',
+        'state',
+      ]),
+      phenotypes: relayStylePagination(['hpoId', 'name']),
+    },
+  },
 };
-
-/*
- * Local Variable GQL, reactive variables
- */
