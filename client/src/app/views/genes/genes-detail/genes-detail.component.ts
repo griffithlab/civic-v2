ee90b5945475665f3ec30a32d9ae1d180d942a3a
Josh McMichael <josh@jmcmichael.com>
2021-07-19 12:14:40 -0500

    gene-details page header uses new mock curator-avatars component

--- client/src/app/views/genes/genes-detail/genes-detail.component.ts
+++ client/src/app/views/genes/genes-detail/genes-detail.component.ts
@@ -28,16 +28,12 @@ export class GenesDetailComponent implements OnDestroy {
   getRouteLabel: (label: string) => string;
   routeSub: Subscription;
   routeLabelSub?: Subscription;
-  curators: any[];
-  editors: any[];
 
   constructor(
     private gql: GeneDetailGQL,
     private viewerService: ViewerService,
     private route: ActivatedRoute
   ) {
-    this.curators = getCurators();
-    this.editors = getEditors();
     this.getRouteLabel = (label) => label;
     this.routeSub = this.route.params.subscribe((params) => {
       let observable = this.gql.watch({ geneId: +params.geneId }).valueChanges;
@@ -76,126 +72,4 @@ export class GenesDetailComponent implements OnDestroy {
       this.routeLabelSub.unsubscribe();
     }
   }
-}
-
-export function getCurators(): any[] {
-  return [
-    {
-      id: 6,
-      displayName: 'Edward Butcher',
-      username: 'ebutcher',
-      profileImagePath: 'https://randomuser.me/api/portraits/lego/2.jpg',
-      role: 'curator'
-    },
-    {
-      id: 22,
-      displayName: 'Swansen Geronimo',
-      username: 'sgeronimo',
-      profileImagePath: 'https://minimaltoolkit.com/images/randomdata/male/45.jpg',
-      role: 'curator'
-    },
-    {
-      id: 6,
-      displayName: 'Winsten Lozano',
-      username: 'ebutcher',
-      profileImagePath: 'https://randomuser.me/api/portraits/lego/6.jpg',
-      role: 'curator'
-    },
-    {
-      id: 66,
-      displayName: 'ebutcher',
-      username: 'ebutcher',
-      profileImagePath: 'https://minimaltoolkit.com/images/randomdata/female/65.jpg',
-      role: 'curator'
-    },
-    {
-      id: 22,
-      displayName: 'Shena Zhang',
-      username: 'szhang',
-      profileImagePath: 'https://minimaltoolkit.com/images/randomdata/female/66.jpg',
-      role: 'curator'
-    },
-    {
-      id: 6,
-      displayName: 'Jed Lynn',
-      username: 'jlynn',
-      profileImagePath: 'https://randomuser.me/api/portraits/lego/9.jpg',
-      role: 'curator'
-    },
-    {
-      id: 26,
-      displayName: 'Brentan Hull',
-      username: 'bhull',
-      profileImagePath: 'https://randomuser.me/api/portraits/lego/7.jpg',
-      role: 'curator'
-    },
-    {
-      id: 689,
-      displayName: 'Nikolas Morrison',
-      username: 'nmorrison',
-      profileImagePath: 'https://minimaltoolkit.com/images/randomdata/male/44.jpg',
-      role: 'curator'
-    },
-    {
-      id: 223,
-      displayName: 'Demitri Webster',
-      username: 'dwebster',
-      profileImagePath: 'https://minimaltoolkit.com/images/randomdata/male/84.jpg',
-      role: 'curator'
-    },
-  ]
-}
-
-export function getEditors(): any[] {
-  return [
-    {
-      id: 76,
-      displayName: 'Brieann Newton',
-      username: 'bnewton',
-      profileImagePath: 'https://randomuser.me/api/portraits/lego/8.jpg',
-      role: 'curator'
-    },
-    {
-      id: 222,
-      displayName: 'Anslie Macdonald',
-      username: 'amacdonald',
-      profileImagePath: 'https://minimaltoolkit.com/images/randomdata/female/94.jpg',
-      role: 'curator'
-    },
-    {
-      id: 22,
-      displayName: 'Duglas Graham',
-      username: 'sgeronimo',
-      profileImagePath: 'https://minimaltoolkit.com/images/randomdata/male/22.jpg',
-      role: 'admin'
-    },
-    {
-      id: 6,
-      displayName: 'Murielle Osborn',
-      username: 'mosborn',
-      profileImagePath: 'https://minimaltoolkit.com/images/randomdata/female/22.jpg',
-      role: 'editor'
-    },
-    {
-      id: 22,
-      displayName: 'Claire Morse',
-      username: 'cmorse',
-      profileImagePath: 'https://minimaltoolkit.com/images/randomdata/female/75.jpg',
-      role: 'editor'
-    },
-    {
-      id: 262,
-      displayName: 'Claire Morse',
-      username: 'cmorse',
-      profileImagePath: 'https://randomuser.me/api/portraits/lego/2.jpg',
-      role: 'editor'
-    },
-    {
-      id: 16,
-      displayName: 'Perkins Christensen',
-      username: 'pchristens',
-      profileImagePath: 'https://minimaltoolkit.com/images/randomdata/male/47.jpg',
-      role: 'editor'
-    },
-  ]
 }
