85bd4dd4894307e1bb6deda2bc7ceb313a06740f
Josh McMichael <josh@jmcmichael.com>
2021-07-26 11:33:14 -0500

    added breadcrumb labels to top level view routes

--- client/src/app/layout/layout-routing.module.ts
+++ client/src/app/layout/layout-routing.module.ts
@@ -16,14 +16,53 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       // Primary Views
-      { path: 'assertions', loadChildren: () => import('@app/views/assertions/assertions.module').then(m => m.AssertionsModule) },
-      { path: 'clinical-trials' , loadChildren: () => import('@app/views/clinical-trials/clinical-trials.module').then(m => m.ClinicalTrialsModule)},
-      { path: 'comments', loadChildren: () => import('@app/views/comments/comments.module').then(m => m.CommentsModule) },
-      { path: 'community', loadChildren: () => import('@app/views/community/community.module').then(m => m.CommunityModule) },
-      { path: 'diseases', loadChildren: () => import('@app/views/diseases/diseases.module').then(m => m.DiseasesModule) },
-      { path: 'drugs', loadChildren: () => import('@app/views/drugs/drugs.module').then(m => m.DrugsModule) },
-      { path: 'evidence', loadChildren: () => import('@app/views/evidence/evidence.module').then(m => m.EvidenceModule) },
-      { path: 'flags', loadChildren: () => import('@app/views/flags/flags.module').then(m => m.FlagsModule) },
+      { path: 'assertions',
+        loadChildren: () => import('@app/views/assertions/assertions.module').then(m => m.AssertionsModule),
+        data: {
+          breadcrumb: 'Assertions'
+        }
+      },
+      { path: 'clinical-trials' ,
+        loadChildren: () => import('@app/views/clinical-trials/clinical-trials.module').then(m => m.ClinicalTrialsModule),
+        data: {
+          breadcrumb: 'Clinical Trials'
+        }
+      },
+      { path: 'comments', loadChildren: () => import('@app/views/comments/comments.module').then(m => m.CommentsModule),
+        data: {
+          breadcrumb: 'Comments'
+        }
+      },
+      { path: 'community',
+        loadChildren: () => import('@app/views/community/community.module').then(m => m.CommunityModule),
+        data: {
+          breadcrumb: 'Community'
+        }
+      },
+      { path: 'diseases',
+        loadChildren: () => import('@app/views/diseases/diseases.module').then(m => m.DiseasesModule),
+        data: {
+          breadcrumb: 'Diseases'
+        }
+      },
+      { path: 'drugs',
+        loadChildren: () => import('@app/views/drugs/drugs.module').then(m => m.DrugsModule),
+        data: {
+          breadcrumb: 'Drugs'
+        }
+      },
+      { path: 'evidence',
+        loadChildren: () => import('@app/views/evidence/evidence.module').then(m => m.EvidenceModule),
+        data: {
+          breadcrumb: 'Evidence'
+        }
+      },
+      { path: 'flags',
+        loadChildren: () => import('@app/views/flags/flags.module').then(m => m.FlagsModule),
+        data: {
+          breadcrumb: 'Flags'
+        }
+      },
       {
         path: 'genes',
         loadChildren: () => import('@app/views/genes/genes.module').then(m => m.GenesModule),
@@ -31,20 +70,79 @@ const routes: Routes = [
           breadcrumb: 'Genes'
         }
       },
-      { path: 'organizations', loadChildren: () => import('@app/views/organizations/organizations.module').then(m => m.OrganizationsModule)},
-      { path: 'phenotypes', loadChildren: () => import('@app/views/phenotypes/phenotypes.module').then(m => m.PhenotypesModule) },
-      { path: 'revisions', loadChildren: () => import('@app/views/revisions/revisions.module').then(m => m.RevisionsModule) },
-      { path: 'sources', loadChildren: () => import('@app/views/sources/sources.module').then(m => m.SourcesModule) },
-      { path: 'users', loadChildren: () => import('@app/views/users/users.module').then(m => m.UsersModule)},
-      { path: 'variant-groups', loadChildren: () => import('@app/views/variant-groups/variant-groups.module').then(m => m.VariantGroupsModule) },
-      { path: 'variants', loadChildren: () => import('@app/views/variants/variants.module').then(m => m.VariantsModule) },
-      { path: 'variant-types', loadChildren: () => import('@app/views/variant-types/variant-types.module').then(m => m.VariantTypesModule) },
+      { path: 'organizations',
+        loadChildren: () => import('@app/views/organizations/organizations.module').then(m => m.OrganizationsModule),
+        data: {
+          breadcrumb: 'Organizations'
+        }
+      },
+      { path: 'phenotypes',
+        loadChildren: () => import('@app/views/phenotypes/phenotypes.module').then(m => m.PhenotypesModule),
+        data: {
+          breadcrumb: 'Phenotypes'
+        }
+      },
+      { path: 'revisions',
+        loadChildren: () => import('@app/views/revisions/revisions.module').then(m => m.RevisionsModule),
+        data: {
+          breadcrumb: 'Revisions'
+        }
+      },
+      { path: 'sources',
+        loadChildren: () => import('@app/views/sources/sources.module').then(m => m.SourcesModule),
+        data: {
+          breadcrumb: 'Sources'
+        }
+      },
+      { path: 'users',
+        loadChildren: () => import('@app/views/users/users.module').then(m => m.UsersModule),
+        data: {
+          breadcrumb: 'Users'
+        }
+      },
+      { path: 'variant-groups',
+        loadChildren: () => import('@app/views/variant-groups/variant-groups.module').then(m => m.VariantGroupsModule),
+        data: {
+          breadcrumb: 'Variant Groups'
+        }},
+      { path: 'variants',
+        loadChildren: () => import('@app/views/variants/variants.module').then(m => m.VariantsModule),
+        data: {
+          breadcrumb: 'Variants'
+        }
+      },
+      { path: 'variant-types',
+        loadChildren: () => import('@app/views/variant-types/variant-types.module').then(m => m.VariantTypesModule),
+        data: {
+          breadcrumb: 'Variant Types'
+        }
+      },
 
       // Other App Views
-      { path: 'welcome', loadChildren: () => import('@app/views/welcome/welcome.module').then(m => m.WelcomeModule) },
-      { path: 'statistics', loadChildren: () => import('@app/views/statistics/statistics.module').then(m => m.StatisticsModule) },
-      { path: 'releases', loadChildren: () => import('@app/views/releases/releases.module').then(m => m.ReleasesModule) },
-      { path: 'pages', loadChildren: () => import('@app/views/pages/pages.module').then(m => m.PagesModule) },
+      { path: 'welcome',
+        loadChildren: () => import('@app/views/welcome/welcome.module').then(m => m.WelcomeModule),
+        data: {
+          breadcrumb: 'Welcome to CIViC'
+        }
+      },
+      { path: 'statistics',
+        loadChildren: () => import('@app/views/statistics/statistics.module').then(m => m.StatisticsModule),
+        data: {
+          breadcrumb: 'Statistics'
+        }
+      },
+      { path: 'releases',
+        loadChildren: () => import('@app/views/releases/releases.module').then(m => m.ReleasesModule),
+        data: {
+          breadcrumb: 'Releases'
+        }
+      },
+      { path: 'pages',
+        loadChildren: () => import('@app/views/pages/pages.module').then(m => m.PagesModule),
+        data: {
+          breadcrumb: 'Pages'
+        }
+      },
     ]
   },
 ];
