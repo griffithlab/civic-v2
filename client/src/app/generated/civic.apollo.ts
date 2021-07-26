707397c74f2e2a521af81ee2efd9571774518f66
Josh McMichael <josh@jmcmichael.com>
2021-07-26 11:08:48 -0500

    added variantAliases to variant-details query

--- client/src/app/generated/civic.apollo.ts
+++ client/src/app/generated/civic.apollo.ts
@@ -4728,7 +4728,7 @@ export type VariantDetailQuery = (
 
 export type VariantDetailFieldsFragment = (
   { __typename: 'Variant' }
-  & Pick<Variant, 'id' | 'name'>
+  & Pick<Variant, 'id' | 'name' | 'variantAliases'>
   & { gene: (
     { __typename: 'Gene' }
     & Pick<Gene, 'id' | 'name'>
@@ -5619,6 +5619,7 @@ export const VariantDetailFieldsFragmentDoc = gql`
     id
     name
   }
+  variantAliases
   flags(state: OPEN) {
     totalCount
   }
