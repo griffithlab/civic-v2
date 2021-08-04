34a7002d812b29bd6ca6b5be768ea7d7ebdd2224
Josh McMichael <josh@jmcmichael.com>
2021-08-04 10:07:17 -0500

    added variant groups revisions, flags pages & components

--- client/src/app/views/variant-groups/variant-groups-revisions/variant-groups-revisions.component.ts
+++ client/src/app/views/variant-groups/variant-groups-revisions/variant-groups-revisions.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Maybe, Organization } from '@app/generated/civic.apollo';
+import {
+  VariantGroupsRevisionsService,
+  SelectableFieldName,
+  UniqueRevisor
+} from './variant-groups-revisions.service';
 
 @Component({
   selector: 'cvc-variant-groups-revisions',
   templateUrl: './variant-groups-revisions.component.html',
-  styleUrls: ['./variant-groups-revisions.component.less']
+  styleUrls: ['./variant-groups-revisions.component.less'],
+  providers: [VariantGroupsRevisionsService]
 })
 export class VariantGroupsRevisionsComponent implements OnInit {
+  service: VariantGroupsRevisionsService;
+  mostRecentOrg: Maybe<Organization>;
+  variantGroupId: number
 
-  constructor() { }
+  constructor(
+    private variantGroupsRevisionsService: VariantGroupsRevisionsService,
+    private route: ActivatedRoute) {
+
+    this.variantGroupId = +this.route.snapshot.params['variantGroupId'];
+    this.service = variantGroupsRevisionsService;
+  }
 
   ngOnInit(): void {
+    this.service.createQuery({ variantGroupId: this.variantGroupId });
+  }
+
+  onFieldNameSelected(field: Maybe<SelectableFieldName>) {
+    this.service.fieldNameSelected(field)
+  }
+
+  onRevisorSelected(user: Maybe<UniqueRevisor>) {
+    this.service.revisorSelected(user)
+  }
+
+  selectOrg(org: Organization): void {
+    this.mostRecentOrg = org;
   }
 
 }
