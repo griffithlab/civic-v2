a222ae46fae70e3394126dd7c0cb497525c607e5
Josh McMichael <josh@jmcmichael.com>
2021-08-10 08:26:57 -0500

    disease table shows entity count icons

--- client/src/app/components/diseases/diseases-table/diseases-table.module.ts
+++ client/src/app/components/diseases/diseases-table/diseases-table.module.ts
@@ -6,12 +6,16 @@ import { CvcClearableInputFilterModule } from '@app/components/shared/clearable-
 import { CvcLinkTagModule } from '@app/components/shared/cvc-link-tag/cvc-link-tag.module';
 import { ReactiveComponentModule } from '@ngrx/component';
 import { CvcDiseaseTagModule } from '../cvc-disease-tag/cvc-disease-tag.module';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
 @NgModule({
   declarations: [CvcDiseasesTableComponent],
   imports: [
     CommonModule,
     ReactiveComponentModule,
+    NzIconModule,
+    NzToolTipModule,
     NzTableModule,
     CvcClearableInputFilterModule,
     CvcLinkTagModule,
