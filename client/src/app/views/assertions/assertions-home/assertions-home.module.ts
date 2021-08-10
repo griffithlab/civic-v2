196b20474be071668ea3cdd76e75763cfd69905a
Josh McMichael <josh@jmcmichael.com>
2021-08-09 19:40:09 -0500

    assertion-home styles, icon

--- client/src/app/views/assertions/assertions-home/assertions-home.module.ts
+++ client/src/app/views/assertions/assertions-home/assertions-home.module.ts
@@ -7,12 +7,14 @@ import { NzGridModule } from 'ng-zorro-antd/grid';
 import { ReactiveComponentModule } from '@ngrx/component';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
 import { CvcAssertionsTableModule } from '@app/components/assertions/assertions-table/assertions-table.module';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   declarations: [AssertionsHomePage],
   imports: [
     CommonModule,
     ReactiveComponentModule,
+    NzIconModule,
     NzTypographyModule,
     NzPageHeaderModule,
     NzGridModule,
