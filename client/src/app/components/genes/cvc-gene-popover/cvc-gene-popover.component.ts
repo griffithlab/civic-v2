d179af882834cb2b46eed114762428a436e4cb3a
Josh McMichael <josh@jmcmichael.com>
2021-08-11 17:13:15 -0500

    popover tag rename

--- client/src/app/components/genes/cvc-gene-popover/cvc-gene-popover.component.ts
+++ client/src/app/components/genes/cvc-gene-popover/cvc-gene-popover.component.ts
@@ -4,7 +4,7 @@ import { map } from 'rxjs/operators'
 import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'cvc-gene-popover-r',
+  selector: 'cvc-gene-popover',
   templateUrl: './cvc-gene-popover.component.html',
   styleUrls: ['./cvc-gene-popover.component.less']
 })
