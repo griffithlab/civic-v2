aff252caeb99b3bad7360022c8615a3f2602c73c
Josh McMichael <josh@jmcmichael.com>
2021-08-15 20:51:14 -0500

    cleaned up app.module

--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -4,15 +4,12 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 
-import { IconsProviderModule } from '@app/icons-provider.module';
-
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { NzPopoverModule } from 'ng-zorro-antd/popover';
-
 
 import { LayoutComponentsModule } from '@app/components/layout/layout-components.module';
 
@@ -33,7 +30,6 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
     NzToolTipModule,
     NzTypographyModule,
     NgxJsonViewerModule,
-    IconsProviderModule,
     LayoutComponentsModule,
 
     CvcQuicksearchModule,
