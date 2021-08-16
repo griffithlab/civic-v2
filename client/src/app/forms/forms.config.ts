5e9c2afd90668b77050a0383ab16c97e6437bc25
Josh McMichael <josh@jmcmichael.com>
2021-08-16 18:19:04 -0500

    moved multi-field to /forms/shared/types/

--- client/src/app/forms/forms.config.ts
+++ client/src/app/forms/forms.config.ts
@@ -1,6 +1,6 @@
 import { defaultValidationMessages } from './validators/default.validators';
-import { MultiFieldType } from './types/multi-field/multi-field.type';
 import { CommentTextareaType } from './comments/types/comment-textarea/comment-textarea.type';
+import { MultiFieldType } from './shared/types/multi-field/multi-field.type';
 
 export const formlyConfig = {
   validationMessages: defaultValidationMessages,
--- client/src/app/forms/genes/forms/gene-revise-form/gene-revise-form.module.ts
+++ client/src/app/forms/genes/forms/gene-revise-form/gene-revise-form.module.ts
@@ -11,7 +11,7 @@ import { NzGridModule } from 'ng-zorro-antd/grid';
 import { CvcFormErrorsAlertModule } from '@app/forms/shared/components/form-errors-alert/form-errors-alert.module';
 import { CvcFormButtonsModule } from '@app/forms/shared/components/form-buttons/form-buttons.module';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { MultiFieldModule } from '@app/forms/types/multi-field/multi-field.module';
+import { MultiFieldModule } from '@app/forms/shared/types/multi-field/multi-field.module';
 import { SourceInputModule } from '@app/forms/sources/types/source-input/source-input.module';
 
 @NgModule({
