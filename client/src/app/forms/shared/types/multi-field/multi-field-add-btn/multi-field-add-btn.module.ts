50ef5a3ab0f68c0c48e1599b79fec70463de260c
Josh McMichael <josh@jmcmichael.com>
2021-09-02 12:32:35 -0500

    added type, component for multi-field add button type

---
+++ client/src/app/forms/shared/types/multi-field/multi-field-add-btn/multi-field-add-btn.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MultiFieldAddBtnComponent } from './multi-field-add-btn.type';
+
+@NgModule({
+  declarations: [MultiFieldAddBtnComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [MultiFieldAddBtnComponent]
+})
+export class MultiFieldAddBtnModule { }
---
+++ client/src/app/forms/shared/types/multi-field/multi-field-add-btn/multi-field-add-btn.type.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cvc-multi-field-add-btn',
+  templateUrl: './multi-field-add-btn.type.html',
+  styleUrls: ['./multi-field-add-btn.type.less']
+})
+export class MultiFieldAddBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
