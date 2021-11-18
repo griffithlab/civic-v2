c0fc377aac24d25b2daf8c3cc6d54b66310c7de0
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-11-18 11:52:35 -0600

    Update revisions and flags pages to disable button when user isn't logged in

--- client/src/app/forms/flags/forms/flag-add/flag-add.module.ts
+++ client/src/app/forms/flags/forms/flag-add/flag-add.module.ts
@@ -9,16 +9,21 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { CvcFormButtonsModule } from '@app/forms/shared/components/form-buttons/form-buttons.module';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { ReactiveComponentModule } from '@ngrx/component';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { CvcPipesModule } from '@app/core/pipes/pipes.module';
 
 @NgModule({
   declarations: [CvcFlagAddForm],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    ReactiveComponentModule,
     NzFormModule,
     NzButtonModule,
     NzCardModule,
     NzAlertModule,
+    NzToolTipModule,
     FormlyModule.forChild(),
     CvcOrgSelectorBtnGroupModule,
     CvcFormButtonsModule,
