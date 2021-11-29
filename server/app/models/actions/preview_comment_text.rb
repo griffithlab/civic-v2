296e341201ec2d12f148097d47c9d036403c1b66
Adam Coffman <acoffman@wustl.edu>
2021-11-29 14:33:32 -0600

    autolink and format user comments

--- server/app/models/actions/preview_comment_text.rb
+++ server/app/models/actions/preview_comment_text.rb
@@ -1,16 +1,27 @@
+require 'rinku'
+require 'sanitize'
+
 module Actions
   class PreviewCommentText
     def self.get_segments(text:)
-      mention_segments = Actions::ExtractMentions.new(text)
+      linked = Rinku.auto_link(text, :all, 'target="_blank"')
+      sanitized = Sanitize.fragment(linked, Sanitize::Config::BASIC)
+       .gsub("\n", "<br/>")
+
+      mention_segments = Actions::ExtractMentions.new(sanitized)
         .perform
         .segments
       reference_segments = Actions::ExtractReferences.new(mention_segments)
         .perform
         .segments
 
-      #TODO sanitize, markdown, make links, etc
-
-      return reference_segments
+      return reference_segments.map do |segment|
+        if segment.is_a?(String)
+          segment.strip + " "
+        else
+          segment
+        end
+      end
     end
   end
 end
