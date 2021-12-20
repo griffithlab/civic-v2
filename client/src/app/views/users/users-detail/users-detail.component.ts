1a228df4e66834b1449da26446a4591b8f4310a1
Adam Coffman <acoffman@wustl.edu>
2021-12-20 14:21:27 -0600

    notifications tab no longer requires a refresh to render

--- client/src/app/views/users/users-detail/users-detail.component.ts
+++ client/src/app/views/users/users-detail/users-detail.component.ts
@@ -8,91 +8,92 @@ import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { RouteableTab } from "@app/components/shared/tab-navigation/tab-navigation.component";
 
 @Component({
-    selector: 'users-detail',
-    templateUrl: './users-detail.component.html',
-    styleUrls: ['./users-detail.component.less']
+  selector: 'users-detail',
+  templateUrl: './users-detail.component.html',
+  styleUrls: ['./users-detail.component.less'],
 })
-
 export class UsersDetailComponent implements OnDestroy {
-    queryRef?: QueryRef<UserDetailQuery, UserDetailQueryVariables>;
-    user$?: Observable<Maybe<UserDetailFieldsFragment>>;
-    loading$?: Observable<boolean>;
-    viewer$?: Observable<Viewer>;
-    ownProfile$ = new BehaviorSubject<boolean>(false);
-    uploadError = false
-    updateSuccess = false
+  queryRef?: QueryRef<UserDetailQuery, UserDetailQueryVariables>;
+  user$?: Observable<Maybe<UserDetailFieldsFragment>>;
+  loading$?: Observable<boolean>;
+  viewer$?: Observable<Viewer>;
+  ownProfile$ = new BehaviorSubject<boolean>(false);
+  uploadError = false;
+  updateSuccess = false;
 
-    routeSub: Subscription;
-    viewerSub?: Subscription
+  routeSub: Subscription;
+  viewerSub?: Subscription;
 
-    tabs: RouteableTab[]
+  tabs$: BehaviorSubject<RouteableTab[]>;
 
-    constructor(private gql: UserDetailGQL, private viewerService: ViewerService, private route: ActivatedRoute) {
+  defaultTabs: RouteableTab[] = [
+    {
+      routeName: 'activity',
+      tabLabel: 'Activity',
+      iconName: 'civic:event',
+    },
+    {
+      routeName: 'evidence',
+      tabLabel: 'Evidence Items',
+      iconName: 'civic:evidence',
+    },
+    {
+      routeName: 'assertions',
+      tabLabel: 'Assertions',
+      iconName: 'civic:assertion',
+    },
+    {
+      routeName: 'source-suggestions',
+      tabLabel: 'Source Suggestions',
+      iconName: 'file-add',
+    },
+    {
+      routeName: 'badges',
+      tabLabel: 'Badges',
+      iconName: 'safety-certificate',
+    },
+  ];
 
-        this.routeSub = this.route.params.subscribe((params) => {
-            this.queryRef = this.gql.watch({userId: +params.userId});
-            let observable = this.queryRef.valueChanges
-            this.loading$ = observable.pipe(
-                pluck('loading'),
-                startWith(true));
+  constructor(
+    private gql: UserDetailGQL,
+    private viewerService: ViewerService,
+    private route: ActivatedRoute
+  ) {
+    this.tabs$ = new BehaviorSubject(this.defaultTabs);
 
-            this.user$ = observable.pipe(
-            pluck('data', 'user'));
+    this.routeSub = this.route.params.subscribe((params) => {
+      this.queryRef = this.gql.watch({ userId: +params.userId });
+      let observable = this.queryRef.valueChanges;
+      this.loading$ = observable.pipe(pluck('loading'), startWith(true));
 
-            this.viewerSub = this.viewerService.viewer$.subscribe((v => {
-                if (v.id === +params.userId) {
-                    let notificationTab: RouteableTab = {
-                        routeName: 'notifications',
-                        tabLabel: 'Notifications',
-                        iconName: 'bell'
+      this.user$ = observable.pipe(pluck('data', 'user'));
 
-                    }
-                    this.tabs.push(notificationTab)
-                    this.ownProfile$.next(true)
-                }
-            }))
-        })
+      this.viewerSub = this.viewerService.viewer$.subscribe((v) => {
+        if (v.id === +params.userId) {
+          let notificationTab: RouteableTab = {
+            routeName: 'notifications',
+            tabLabel: 'Notifications',
+            iconName: 'bell',
+          };
 
-        this.tabs = [
-            {
-                routeName: 'activity',
-                tabLabel: 'Activity',
-                iconName: 'civic:event'
-            },
-            {
-                routeName: 'evidence',
-                tabLabel: 'Evidence Items',
-                iconName: 'civic:evidence'
-            },
-            {
-                routeName: 'assertions',
-                tabLabel: 'Assertions',
-                iconName: 'civic:assertion'
-            },
-            {
-                routeName: 'source-suggestions',
-                tabLabel: 'Source Suggestions',
-                iconName: 'file-add'
-            },
-            {
-                routeName: 'badges',
-                tabLabel: 'Badges',
-                iconName: 'safety-certificate'
-            },
-        ]
-    }
-
-    ngOnDestroy() {
-        this.routeSub.unsubscribe();
-        this.viewerSub?.unsubscribe();
-    }
-
-    profileUploadComplete(success: boolean) {
-        if (success) {
-            this.queryRef?.refetch()
-            this.updateSuccess = true
-        } else {
-            this.uploadError = true
+          this.tabs$.next([...this.defaultTabs, notificationTab]);
+          this.ownProfile$.next(true);
         }
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
+    this.viewerSub?.unsubscribe();
+  }
+
+  profileUploadComplete(success: boolean) {
+    if (success) {
+      this.queryRef?.refetch();
+      this.updateSuccess = true;
+    } else {
+      this.uploadError = true;
     }
+  }
 }
