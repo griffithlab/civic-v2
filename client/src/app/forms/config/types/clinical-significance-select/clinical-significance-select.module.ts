709b237dda241b186fd5a60a81cbf02bf2ad1ae7
Josh McMichael <josh@jmcmichael.com>
2022-03-01 20:44:39 -0600

    tweaked formly config

--- client/src/app/forms/config/types/clinical-significance-select/clinical-significance-select.module.ts
+++ client/src/app/forms/config/types/clinical-significance-select/clinical-significance-select.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlySelectModule } from '@ngx-formly/core/select';
+import { FormlyNzFormFieldModule } from '@ngx-formly/ng-zorro-antd/form-field';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 import {
   clinicalSignificanceSelectTypeOption,
   optionValidationMessage,
-  optionValidator,
+  optionValidator
 } from './clinical-significance-select.type';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { FormlyNzFormFieldModule } from '@ngx-formly/ng-zorro-antd/form-field';
 
 @NgModule({
   imports: [
