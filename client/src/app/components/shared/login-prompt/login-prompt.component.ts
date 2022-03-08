dc8c1cf01d0db1446853b8352922026ffaaa9f67
Susanna Kiwala <susanna.kiwala@wustl.edu>
2022-03-08 12:46:25 -0600

    Add login-prompt component files

---
+++ client/src/app/components/shared/login-prompt/login-prompt.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cvc-login-prompt',
+  templateUrl: './login-prompt.component.html',
+  styleUrls: ['./login-prompt.component.less']
+})
+export class CvcLoginPromptComponent {
+}
