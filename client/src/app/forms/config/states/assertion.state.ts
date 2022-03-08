d955f7817ef0e47ab1f1464979cad9b131b64d25
Josh McMichael <josh@jmcmichael.com>
2022-03-08 12:45:23 -0600

    abstracted entity assertion entity state class

--- client/src/app/forms/config/states/assertion.state.ts
+++ client/src/app/forms/config/states/assertion.state.ts
@@ -5,33 +5,19 @@ import {
   Maybe
 } from "@app/generated/civic.apollo";
 import { $enum } from "ts-enum-util";
+import {
+  IEntityState,
+  EntityType,
+  EntityDirection,
+  EntityClinicalSignificance,
+  ValidEntity
+} from "./state.types";
 
-interface ValidAssertionItem {
-  assertionType: AssertionType;
-  clinicalSignificance: AssertionClinicalSignificance[];
-  assertionDirection: AssertionDirection[];
-  requiresDisease: boolean;
-  requiresDrug: boolean;
-  requiresAcmgCodes: boolean;
-  requiresAmpLevel: boolean;
-}
-
-interface IAssertionState {
-  validStates: Map<AssertionType, ValidAssertionItem>;
-  getSignificanceOptions: (et?: AssertionType) => AssertionClinicalSignificance[];
-  getDirectionOptions: (at: AssertionType) => AssertionDirection[];
-  isValidSignificanceOption: (at: AssertionType, cs: AssertionClinicalSignificance) => boolean;
-  requiresDrug: (at: AssertionType) => boolean;
-  requiresDisease: (at: AssertionType) => boolean;
-  requiresAcmgCodes: (at: AssertionType) => boolean;
-  requiresAmpLevel: (at: AssertionType) => boolean;
-}
-
-class AssertionState implements IAssertionState {
-  validStates = new Map<AssertionType, ValidAssertionItem>();
+class AssertionState implements IEntityState {
+  validStates = new Map<EntityType, ValidEntity>();
   constructor() {
     this.validStates.set(AssertionType.Predictive, {
-      assertionType: AssertionType.Predictive,
+      entityType: AssertionType.Predictive,
       clinicalSignificance: [
         AssertionClinicalSignificance.Sensitivityresponse,
         AssertionClinicalSignificance.Resistance,
@@ -39,7 +25,7 @@ class AssertionState implements IAssertionState {
         AssertionClinicalSignificance.ReducedSensitivity,
         AssertionClinicalSignificance.Na,
       ],
-      assertionDirection: [
+      entityDirection: [
         AssertionDirection.Supports,
         AssertionDirection.DoesNotSupport
       ],
@@ -50,12 +36,12 @@ class AssertionState implements IAssertionState {
     });
 
     this.validStates.set(AssertionType.Diagnostic, {
-      assertionType: AssertionType.Diagnostic,
+      entityType: AssertionType.Diagnostic,
       clinicalSignificance: [
         AssertionClinicalSignificance.Positive,
         AssertionClinicalSignificance.Negative,
       ],
-      assertionDirection: [
+      entityDirection: [
         AssertionDirection.Supports,
         AssertionDirection.DoesNotSupport
       ],
@@ -66,13 +52,13 @@ class AssertionState implements IAssertionState {
     });
 
     this.validStates.set(AssertionType.Prognostic, {
-      assertionType: AssertionType.Prognostic,
+      entityType: AssertionType.Prognostic,
       clinicalSignificance: [
         AssertionClinicalSignificance.BetterOutcome,
         AssertionClinicalSignificance.PoorOutcome,
         AssertionClinicalSignificance.Na
       ],
-      assertionDirection: [
+      entityDirection: [
         AssertionDirection.Supports,
         AssertionDirection.DoesNotSupport
       ],
@@ -83,7 +69,7 @@ class AssertionState implements IAssertionState {
     });
 
     this.validStates.set(AssertionType.Predisposing, {
-      assertionType: AssertionType.Predisposing,
+      entityType: AssertionType.Predisposing,
       clinicalSignificance: [
         AssertionClinicalSignificance.Pathogenic,
         AssertionClinicalSignificance.LikelyPathogenic,
@@ -91,7 +77,7 @@ class AssertionState implements IAssertionState {
         AssertionClinicalSignificance.LikelyBenign,
         AssertionClinicalSignificance.UncertainSignificance,
       ],
-      assertionDirection: [
+      entityDirection: [
         AssertionDirection.Supports,
         AssertionDirection.DoesNotSupport,
       ],
@@ -103,50 +89,54 @@ class AssertionState implements IAssertionState {
 
   }
 
+  getTypeOptions = (): EntityType[] => {
+    return $enum(AssertionType).map(value => value);
+  }
+
   // call with no argument to get all significance options
-  getSignificanceOptions = (at: Maybe<AssertionType>): AssertionClinicalSignificance[] => {
+  getSignificanceOptions = (at: Maybe<EntityType>): EntityClinicalSignificance[] => {
     if (!at) { return $enum(AssertionClinicalSignificance).map(value => value); }
     const state = this.validStates.get(at);
     return state?.clinicalSignificance || [];
   }
 
-  isValidSignificanceOption = (at: AssertionType,
-    cs: AssertionClinicalSignificance): boolean => {
+  isValidSignificanceOption = (at: EntityType,
+    cs: EntityClinicalSignificance): boolean => {
     const state = this.validStates.get(at);
     if (!state) { return true; }
     return state.clinicalSignificance.includes(cs);
   }
 
-  getDirectionOptions = (at: AssertionType): AssertionDirection[] => {
+  getDirectionOptions = (at: EntityType): EntityDirection[] => {
     const state = this.validStates.get(at);
-    return state?.assertionDirection || [];
+    return state?.entityDirection || [];
   }
 
   isValidDirectionOption = (at: AssertionType, ed: AssertionDirection): boolean => {
     const state = this.validStates.get(at);
     if (!state) { return true; }
-    return state.assertionDirection.includes(ed);
+    return state.entityDirection.includes(ed);
   }
 
-  requiresDrug = (at: AssertionType): boolean => {
+  requiresDrug = (at: EntityType): boolean => {
     const state = this.validStates.get(at);
     return state !== undefined ? state.requiresDrug : true;
   }
 
-  requiresDisease = (at: AssertionType): boolean => {
+  requiresDisease = (at: EntityType): boolean => {
     const state = this.validStates.get(at);
     return state !== undefined ? state.requiresDisease : true;
   }
 
-  requiresAcmgCodes = (at: AssertionType): boolean => {
+  requiresAcmgCodes = (at: EntityType): boolean => {
     const state = this.validStates.get(at);
-    return state !== undefined ? state.requiresAcmgCodes: true;
+    return state !== undefined ? state.requiresAcmgCodes : true;
   }
 
-  requiresAmpLevel = (at: AssertionType): boolean => {
+  requiresAmpLevel = (at: EntityType): boolean => {
     const state = this.validStates.get(at);
-    return state !== undefined ? state.requiresAmpLevel: true;
+    return state !== undefined ? state.requiresAmpLevel : true;
   }
 }
 
-export { AssertionState, IAssertionState };
+export { AssertionState };
---
+++ client/src/app/forms/config/states/state.types.ts
@@ -0,0 +1,39 @@
+import { AssertionClinicalSignificance, AssertionDirection, AssertionType, EvidenceClinicalSignificance, EvidenceDirection, EvidenceType } from "@app/generated/civic.apollo";
+
+export type EntityClinicalSignificance =
+  | EvidenceClinicalSignificance
+  | AssertionClinicalSignificance;
+
+export type EntityType =
+  | EvidenceType
+  | AssertionType
+
+// export type ValidEntity =
+//   | ValidEvidenceItem
+//   | ValidAssertion
+
+export type EntityDirection =
+  | EvidenceDirection
+  | AssertionDirection;
+
+export type ValidEntity = {
+  entityType: EntityType
+  clinicalSignificance: EntityClinicalSignificance[]
+  entityDirection: EntityDirection[]
+  requiresDisease: boolean
+  requiresDrug: boolean
+  requiresAcmgCodes: boolean
+  requiresAmpLevel: boolean
+}
+
+export interface IEntityState {
+  validStates: Map<EntityType, ValidEntity>
+  getTypeOptions: () => EntityType[];
+  getSignificanceOptions: (et?: EntityType) => EntityClinicalSignificance[];
+  getDirectionOptions: (et: EntityType) => EntityDirection[];
+  isValidSignificanceOption: (et: EntityType, cs: EntityClinicalSignificance) => boolean;
+  requiresDrug: (et: EntityType) => boolean;
+  requiresDisease: (et: EntityType) => boolean;
+  requiresAcmgCodes: (et: EntityType) => boolean;
+  requiresAmpLevel: (et: EntityType) => boolean;
+}
