32c9ff6f78df056146d06f1bea971b17e078a06b
Susanna Kiwala <susanna.kiwala@wustl.edu>
2022-03-24 09:23:35 -0500

    Hook up the about/help/contact pages

--- client/src/app/views/pages/pages-routing.module.ts
+++ client/src/app/views/pages/pages-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PagesAboutComponent } from './pages-about/pages-about.component';
 import { PagesContactComponent } from './pages-contact/pages-contact.component';
-import { PagesHelpComponent } from './pages-help/pages-help.component';
 import { PagesComponent } from './pages.component';
 
 const routes: Routes = [
@@ -10,14 +8,6 @@ const routes: Routes = [
     path: '',
     component: PagesComponent,
     children: [
-      {
-        path: 'about',
-        component: PagesAboutComponent
-      },
-      {
-        path: 'help',
-        component: PagesHelpComponent
-      },
       {
         path: 'contact',
         component: PagesContactComponent
