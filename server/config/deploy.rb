040d0d65c3f257f9d9541e509f7ec7f9445a3771
Adam Coffman <acoffman@wustl.edu>
2022-03-24 11:54:38 -0500

    preserve storage directory between deploys

--- server/config/deploy.rb
+++ server/config/deploy.rb
@@ -11,7 +11,7 @@ set :deploy_to, '/var/www/civic/'
 
 set :rbenv_type, :user
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/downloads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/downloads', 'storage')
 
 set :repo_tree, 'server'
 
