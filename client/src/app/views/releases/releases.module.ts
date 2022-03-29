9b2070ac0e7fb93a3a0148b3081e8dda2a3a4ae5
Josh McMichael <josh@jmcmichael.com>
2022-03-29 11:46:57 -0500

    refactored releases section

--- client/src/app/views/releases/releases.module.ts
+++ client/src/app/views/releases/releases.module.ts
@@ -1,41 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { IconsProviderModule } from '@app/icons-provider.module';
-import { NzTableModule } from 'ng-zorro-antd/table';
-
-import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
-import { ReactiveComponentModule } from '@ngrx/component';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
-import { NzSpaceModule } from 'ng-zorro-antd/space';
-
+import { NgModule } from '@angular/core';
+import { ReleasesMainModule } from './releases-main/releases-main.module';
 import { ReleasesRoutingModule } from './releases-routing.module';
 import { ReleasesComponent } from './releases.component';
-import { ReleasesMainComponent } from './releases-main/releases-main.component';
-import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { CvcLinkTagModule } from '@app/components/shared/link-tag/link-tag.module';
-import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzAlertModule } from 'ng-zorro-antd/alert';
-
 
 @NgModule({
-  declarations: [ReleasesComponent, ReleasesMainComponent],
+  declarations: [ReleasesComponent],
   imports: [
     CommonModule,
     ReleasesRoutingModule,
-    IconsProviderModule,
-    NzTableModule,
-    NzTypographyModule,
-    NzPageHeaderModule,
-    ReactiveComponentModule,
-    NzGridModule,
-    NzToolTipModule,
-    NzAlertModule,
-    NzSpaceModule,
-    NzCardModule,
-    NzSpaceModule,
-    CvcLinkTagModule
+    ReleasesMainModule,
   ]
 })
 export class ReleasesModule { }
