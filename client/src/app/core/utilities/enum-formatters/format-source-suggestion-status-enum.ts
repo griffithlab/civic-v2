015de3a852ab44b7547c17971e374c9b30c2182b
Josh McMichael <josh@jmcmichael.com>
2022-04-27 17:44:58 -0500

    refactored user-tag for OnPush change detection, virtual scroll cache

---
+++ client/src/app/core/utilities/enum-formatters/format-source-suggestion-status-enum.ts
@@ -0,0 +1,14 @@
+import { SourceSuggestionStatus } from "@app/generated/civic.apollo";
+
+export function formatSourceSuggestionStatusEnum(value: SourceSuggestionStatus): string {
+  switch (value) {
+    case 'NEW':
+      return 'New';
+    case 'CURATED':
+      return 'Curated';
+    case 'REJECTED':
+      return 'Rejected';
+    default:
+      return value;
+  }
+}
