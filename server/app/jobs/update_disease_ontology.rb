466d15c47e53a8c71ce55561b084875daa02973e
Adam Coffman <acoffman@wustl.edu>
2022-05-12 15:50:47 -0500

    port doid update task

---
+++ server/app/jobs/update_disease_ontology.rb
@@ -0,0 +1,40 @@
+require 'tempfile'
+require 'open-uri'
+
+class UpdateDiseaseOntology < ApplicationJob
+  attr_reader :doid_file
+
+  def perform
+    begin
+      create_tempfile
+      download_file
+      import_doid
+    ensure
+      remove_download
+    end
+  end
+
+  def create_tempfile
+    @doid_file = Tempfile.new('doid_download', File.join(Rails.root, 'tmp'))
+  end
+
+  def download_file
+    download_stream = URI.open(latest_doid_path)
+    IO.copy_stream(download_stream, doid_file)
+  end
+
+  def import_doid
+    importer = Importer::DiseaseOntologyMirror.new(doid_file)
+    importer.import
+  end
+
+  def remove_download
+    doid_file.close
+    doid_file.unlink
+  end
+
+  def latest_doid_path
+    "https://raw.githubusercontent.com/DiseaseOntology/HumanDiseaseOntology/main/src/ontology/subsets/DO_cancer_slim.obo"
+  end
+end
+
