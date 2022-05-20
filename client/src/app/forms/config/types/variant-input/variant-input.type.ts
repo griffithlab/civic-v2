82698ba9857a85e72e6b2f7a8359504c7c451c17
Josh McMichael <josh@jmcmichael.com>
2022-05-20 11:56:17 -0500

    updated source selector

--- client/src/app/forms/config/types/variant-input/variant-input.type.ts
+++ client/src/app/forms/config/types/variant-input/variant-input.type.ts
@@ -1,18 +1,15 @@
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-} from '@angular/core';
-import {FormControl } from '@angular/forms';
+import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { NetworkErrorsService } from '@app/core/services/network-errors.service';
 import { MutatorWithState } from '@app/core/utilities/mutation-state-wrapper';
 import { AddVariantGQL, AddVariantMutation, AddVariantMutationVariables, VariantSelectFieldsFragment, VariantSelectQuery, VariantSelectQueryVariables, VariantTypeaheadGQL } from '@app/generated/civic.apollo';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { FieldType, FormlyFieldConfig } from '@ngx-formly/core';
 import { TypeOption } from '@ngx-formly/core/lib/services/formly.config';
 import { QueryRef } from 'apollo-angular';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { map, pluck, takeUntil } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { isNonNulled } from 'rxjs-etc';
+import { filter, map, pluck } from 'rxjs/operators';
 
 interface VariantSelectOption {
   value: number;
@@ -20,6 +17,7 @@ interface VariantSelectOption {
   variant: VariantSelectFieldsFragment;
 }
 
+@UntilDestroy()
 @Component({
   selector: 'cvc-variant-input-type',
   templateUrl: './variant-input.type.html',
@@ -28,10 +26,9 @@ interface VariantSelectOption {
 })
 export class VariantInputType extends FieldType implements OnInit, AfterViewInit {
   formControl!: FormControl;
-  private queryRef!: QueryRef< VariantSelectQuery, VariantSelectQueryVariables >;
+  private queryRef!: QueryRef<VariantSelectQuery, VariantSelectQueryVariables>;
   variants$?: Observable<VariantSelectOption[]>;
 
-  private destroy$ = new Subject();
   success: boolean = false
   errorMessages: string[] = []
   loading: boolean = false
@@ -44,10 +41,10 @@ export class VariantInputType extends FieldType implements OnInit, AfterViewInit
     private variantTypeaheadQuery: VariantTypeaheadGQL,
     private networkErrorService: NetworkErrorsService,
     private addVariantGQL: AddVariantGQL
-  ) { 
-    super(); 
+  ) {
+    super();
 
-    this.addVariantMutator = new MutatorWithState(networkErrorService)
+    this.addVariantMutator = new MutatorWithState(this.networkErrorService)
 
     this.defaultOptions = {
       templateOptions: {
@@ -61,8 +58,8 @@ export class VariantInputType extends FieldType implements OnInit, AfterViewInit
       expressionProperties: {
         'templateOptions.geneId': (model: any, formState: any, ffc?: FormlyFieldConfig) => {
           let mainModel = ffc?.parent?.parent?.model
-          if(mainModel) {
-            if(mainModel.gene && mainModel.gene[0]) {
+          if (mainModel) {
+            if (mainModel.gene && mainModel.gene[0]) {
               return mainModel.gene[0].id
             }
           }
@@ -75,9 +72,12 @@ export class VariantInputType extends FieldType implements OnInit, AfterViewInit
   ngOnInit(): void {
     this.queryRef = this.variantTypeaheadQuery.watch({ name: 'zzzz' });
     // no need to unsubscribe variants$ as ngrxLet in the template does this automatically
-    this.variants$ = this.queryRef.valueChanges.pipe(
-      pluck('data', 'variants', 'nodes'),
-      map((variants: VariantSelectFieldsFragment[]): VariantSelectOption[] => {
+    this.variants$ = this.queryRef
+      .valueChanges
+      .pipe(
+        pluck('data', 'variants', 'nodes'),
+        filter(isNonNulled),
+        map((variants: VariantSelectFieldsFragment[]): VariantSelectOption[] => {
           let variantInputs = variants.map((v: VariantSelectFieldsFragment) => {
             return {
               value: v.id,
@@ -85,13 +85,10 @@ export class VariantInputType extends FieldType implements OnInit, AfterViewInit
               variant: v,
             };
           });
-          
-          return variantInputs;
-        }
-      )
-    );
-  }
 
+          return variantInputs;
+        }));
+  }
 
   ngAfterViewInit() {
     this.to.onSearch = (value: string): void => {
@@ -100,47 +97,43 @@ export class VariantInputType extends FieldType implements OnInit, AfterViewInit
       }
       this.to.searchString = value;
       this.errorMessages = []
-      this.queryRef.refetch({name: value, geneId: this.to.geneId}).then((res) => { 
+      this.queryRef.refetch({ name: value, geneId: this.to.geneId }).then((res) => {
         this.displayAdd$.next(res.data.variants.nodes.filter(d => {
           return d.name.toUpperCase() == value.toUpperCase()
         }).length == 0
-      )})
+        )
+      })
     };
   }
 
-  addVariant(variantName: string): void  {
-    if(variantName && variantName != '' && this.to.geneId) {
+  addVariant(variantName: string): void {
+    if (variantName && variantName != '' && this.to.geneId) {
       let state = this.addVariantMutator.mutate(this.addVariantGQL, { name: variantName, geneId: this.to.geneId }, {},
         (data) => {
-          this.field.formControl?.setValue( {id: data.addVariant.variant.id, name: data.addVariant.variant.name})
+          this.field.formControl?.setValue({ id: data.addVariant.variant.id, name: data.addVariant.variant.name })
           this.to.searchString = '';
           this.to.searchLength = 0;
         })
 
-      state.submitSuccess$.pipe(takeUntil(this.destroy$)).subscribe((res) => {
+      state.submitSuccess$.pipe(untilDestroyed(this)).subscribe((res) => {
         if (res) {
           this.success = true
         }
       })
 
-      state.submitError$.pipe(takeUntil(this.destroy$)).subscribe((errs) => {
+      state.submitError$.pipe(untilDestroyed(this)).subscribe((errs) => {
         if (errs) {
           this.errorMessages = errs
           this.success = false
         }
       })
 
-      state.isSubmitting$.pipe(takeUntil(this.destroy$)).subscribe((loading) => {
+      state.isSubmitting$.pipe(untilDestroyed(this)).subscribe((loading) => {
         this.loading = loading
       })
     }
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-    this.displayAdd$.complete();
-  }
 }
 
 export const variantInputTypeOption: TypeOption = {
