6bd614064ccba14b76daabd419add46de92194e9
Josh McMichael <josh@jmcmichael.com>
2022-07-08 09:27:31 -0500

    source summary now displays PubMed/ASCO Id as link-tag

--- client/src/app/generated/civic.apollo.ts
+++ client/src/app/generated/civic.apollo.ts
@@ -5700,9 +5700,9 @@ export type SourceSummaryQueryVariables = Exact<{
 }>;
 
 
-export type SourceSummaryQuery = { __typename: 'Query', source?: { __typename: 'Source', id: number, displayType: string, title?: string | undefined, abstract?: string | undefined, publicationDate?: string | undefined, citationId: number, fullJournalTitle?: string | undefined, pmcId?: string | undefined, authorString?: string | undefined, clinicalTrials?: Array<{ __typename: 'ClinicalTrial', nctId: string, id: number }> | undefined } | undefined };
+export type SourceSummaryQuery = { __typename: 'Query', source?: { __typename: 'Source', id: number, displayType: string, sourceUrl?: string | undefined, title?: string | undefined, abstract?: string | undefined, publicationDate?: string | undefined, citationId: number, fullJournalTitle?: string | undefined, pmcId?: string | undefined, authorString?: string | undefined, clinicalTrials?: Array<{ __typename: 'ClinicalTrial', nctId: string, id: number }> | undefined } | undefined };
 
-export type SourceSummaryFieldsFragment = { __typename: 'Source', id: number, displayType: string, title?: string | undefined, abstract?: string | undefined, publicationDate?: string | undefined, citationId: number, fullJournalTitle?: string | undefined, pmcId?: string | undefined, authorString?: string | undefined, clinicalTrials?: Array<{ __typename: 'ClinicalTrial', nctId: string, id: number }> | undefined };
+export type SourceSummaryFieldsFragment = { __typename: 'Source', id: number, displayType: string, sourceUrl?: string | undefined, title?: string | undefined, abstract?: string | undefined, publicationDate?: string | undefined, citationId: number, fullJournalTitle?: string | undefined, pmcId?: string | undefined, authorString?: string | undefined, clinicalTrials?: Array<{ __typename: 'ClinicalTrial', nctId: string, id: number }> | undefined };
 
 export type UserDetailQueryVariables = Exact<{
   userId: Scalars['Int'];
@@ -7564,6 +7564,7 @@ export const SourceSummaryFieldsFragmentDoc = gql`
     fragment SourceSummaryFields on Source {
   id
   displayType
+  sourceUrl
   title
   abstract
   publicationDate
