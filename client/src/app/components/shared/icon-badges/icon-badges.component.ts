297c5d8ef8a1eaabc293b239226b25ce38ca15e8
Josh McMichael <josh@jmcmichael.com>
2022-07-13 14:06:40 -0500

    added revision, flag, comment bool inputs

--- client/src/app/components/shared/icon-badges/icon-badges.component.ts
+++ client/src/app/components/shared/icon-badges/icon-badges.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cvc-icon-badges',
@@ -7,6 +7,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CvcIconBadgesComponent {
+  @Input() hasRevisions!: boolean
+  @Input() hasFlags!: boolean
+  @Input() hasComments!: boolean
 
   constructor() { }
 
