f8b8557a65acb1de76c097ca99cc4039082eaed4
Josh McMichael <josh@jmcmichael.com>
2022-07-15 11:41:47 -0500

    updated many civic:entity icon types

--- client/src/app/core/utilities/get-entity-color.ts
+++ client/src/app/core/utilities/get-entity-color.ts
@@ -2,25 +2,35 @@
 // TODO: swap Variant and Molecular Profile colors when MP feature implemented
 export const EntityColors = new Map<string, string>([
   ['Assertion', '#7243b5'],
-  ['CoordinateSystem', '#1db8a9'],
+  ['CoordinateSystem', '#f68f37'],
   ['Disease', '#e62f76'],
-  ['Event', '#ffdb00'],
-  // ['EvidenceItem', '#f68f37'],
   ['EvidenceItem', '#1db8a9'],
   ['Gene', '#07aff0'],
   ['Intervention', '#ac3996'],
-  ['Flag', '#e24759'],
   ['Variant', '#33b358'],
   ['VariantGroup', '#33b358'],
   ['Phenotype', '#2a63b6'],
-  ['Revision', '#f0673a'],
   ['Source', '#f9ba45'],
   ['MolecularProfile', '#b2d44a'],
+
+  // artifacts
+  ['Event', '#ffdb00'],
+  ['Flag', '#e24759'],
+  ['Revision', '#f0673a'],
+  ['Comment', '#44d8ef'],
+
+  // user types
+  ['Editor', '#58a0c4'],
+  ['Curator', '#a0d8f5'],
+  ['Organization', '#3d7b99'],
+
+  // entity state colors
   ['Rejected', '#BBBBBB']
+  
 ])
 
 export function getEntityColor(entity: string): string {
-  let color = '#CCCCCC' // set a default color
+  let color = '#AAAAAA' // default color
   if (entity) {
     const c = EntityColors.get(entity)
     if(c) color = c
--- client/src/app/views/evidence/evidence-detail/evidence-detail.module.ts
+++ client/src/app/views/evidence/evidence-detail/evidence-detail.module.ts
@@ -1,51 +1,54 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EvidenceDetailView } from './evidence-detail.view';
-import { ReactiveComponentModule } from '@ngrx/component';
-import { CvcSectionNavigationModule } from '@app/components/shared/section-navigation/section-navigation.module';
-import { NzSpaceModule } from 'ng-zorro-antd/space';
-import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { CvcGeneTagModule } from '@app/components/genes/gene-tag/gene-tag.module';
-import { CvcFlaggableModule } from '@app/components/flags/flaggable/flaggable.module';
-import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CvcVariantTagModule } from '@app/components/variants/variant-tag/variant-tag.module';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { EvidenceDetailRoutingModule } from './evidence-detail-routing.module';
-import { CvcEntitySubscriptionButtonModule } from '@app/components/shared/entity-subscription-button/entity-subscription-button.module';
-import { CvcRevertEntityButtonModule } from '@app/components/shared/revert-entity-button/revert-entity-button.module';
-import { NzAlertModule } from 'ng-zorro-antd/alert';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { CvcModerateEntityButtonsModule } from '@app/components/shared/moderate-entity-buttons/moderate-entity-button.module';
-import { CvcTabNavigationModule } from '@app/components/shared/tab-navigation/tab-navigation.module';
+import { CvcFlaggableModule } from '@app/components/flags/flaggable/flaggable.module';
+import { CvcGeneTagModule } from '@app/components/genes/gene-tag/gene-tag.module';
 import { CvcContributorAvatarsModule } from '@app/components/shared/contributor-avatars/contributor-avatars.module';
+import { CvcEntitySubscriptionButtonModule } from '@app/components/shared/entity-subscription-button/entity-subscription-button.module';
+import { CvcModerateEntityButtonsModule } from '@app/components/shared/moderate-entity-buttons/moderate-entity-button.module';
+import { CvcRevertEntityButtonModule } from '@app/components/shared/revert-entity-button/revert-entity-button.module';
+import { CvcSectionNavigationModule } from '@app/components/shared/section-navigation/section-navigation.module';
+import { CvcTabNavigationModule } from '@app/components/shared/tab-navigation/tab-navigation.module';
+import { CvcVariantTagModule } from '@app/components/variants/variant-tag/variant-tag.module';
+import { CvcPipesModule } from '@app/core/pipes/pipes.module';
+import { ReactiveComponentModule } from '@ngrx/component';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { EvidenceDetailRoutingModule } from './evidence-detail-routing.module';
+import { EvidenceDetailView } from './evidence-detail.view';
 
 @NgModule({
   declarations: [EvidenceDetailView],
   imports: [
     CommonModule,
     EvidenceDetailRoutingModule,
-    RouterModule,
     ReactiveComponentModule,
-    NzSpaceModule,
-    NzTypographyModule,
-    NzPageHeaderModule,
-    NzButtonModule,
-    NzTagModule,
-    NzIconModule,
+
     NzAlertModule,
+    NzButtonModule,
     NzGridModule,
-    CvcSectionNavigationModule,
-    CvcGeneTagModule,
-    CvcFlaggableModule,
-    CvcVariantTagModule,
+    NzIconModule,
+    NzPageHeaderModule,
+    NzSpaceModule,
+    NzTagModule,
+    NzTypographyModule,
+
+    CvcContributorAvatarsModule,
     CvcEntitySubscriptionButtonModule,
-    CvcRevertEntityButtonModule,
+    CvcFlaggableModule,
+    CvcGeneTagModule,
     CvcModerateEntityButtonsModule,
+    CvcPipesModule,
+    CvcRevertEntityButtonModule,
+    CvcSectionNavigationModule,
     CvcTabNavigationModule,
-    CvcContributorAvatarsModule
+    CvcVariantTagModule,
   ]
 })
 export class EvidenceDetailModule { }
