fb21a03dfef74812b00689b7c6737e49e0e200e6
Josh McMichael <josh@jmcmichael.com>
2022-07-28 09:19:18 -0500

    created cvc-tag-group for gene/variant tag pairs

---
+++ client/src/app/components/shared/tag-group/tag-group.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CvcTagGroupComponent } from './tag-group.component';
+
+@NgModule({
+  declarations: [CvcTagGroupComponent],
+  imports: [
+    CommonModule,
+  ],
+  exports: [CvcTagGroupComponent]
+})
+export class CvcTagGroupModule { }
