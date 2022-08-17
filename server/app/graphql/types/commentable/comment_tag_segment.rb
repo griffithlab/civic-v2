4607c16585e9075767e85a82a44d81d16129e0cf
Adam Coffman <acoffman@wustl.edu>
2022-08-17 13:33:15 -0500

    make revision tags link to pre-filtered set page, regardless of status

--- server/app/graphql/types/commentable/comment_tag_segment.rb
+++ server/app/graphql/types/commentable/comment_tag_segment.rb
@@ -5,5 +5,6 @@ module Types::Commentable
     field :tag_type, Types::Commentable::TaggableEntity, null: false
     field :status, Types::EvidenceStatusType, null: true
     field :link, GraphQL::Types::String, null: false
+    field :revisionset_id, GraphQL::Types::String, null: true
   end
 end
