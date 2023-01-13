79ceebc216f64629ebcc908a80c620a32eb67b8a
Adam Coffman <acoffman@wustl.edu>
2023-01-13 11:20:00 -0600

    scrape abstracts for ash sources

---
+++ server/db/migrate/20230113171643_backfill_ash.rb
@@ -0,0 +1,12 @@
+class BackfillAsh < ActiveRecord::Migration[6.1]
+  def up
+    Source.where(source_type: 'ASH', abstract: nil).find_each do |s|
+      Scrapers::Ash.populate_source_fields(s)
+    end
+
+  end
+
+  def down
+    #no op
+  end
+end
