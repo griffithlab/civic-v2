66965a09af113fec326f34fb9aa0a23dec5aadd3
Joshua McMichael <jmcmicha@wustl.edu>
2023-01-16 18:28:37 -0600

    switched from ngx-timeago to ngx-pipe's timeAgo pipe

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,37 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
+import { BrowserModule } from '@angular/platform-browser'
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core'
+import { registerLocaleData } from '@angular/common'
+import en from '@angular/common/locales/en'
 import {
   HttpClientModule,
   HttpClientXsrfModule,
   HttpClientJsonpModule,
   HttpClient,
-} from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LetModule, PushModule } from '@ngrx/component';
-import { CookieService } from 'ngx-cookie-service';
-import { civicIcons } from '@app/icons-provider.module';
-import { TimeagoFormatter, TimeagoModule } from 'ngx-timeago';
-import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
-import { NgxJsonViewerModule } from 'ngx-json-viewer';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GraphQLModule } from '@app/graphql/graphql.module';
-import { environment } from 'environments/environment';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { CvcNetworkErrorAlertModule } from './components/app/network-error-alert/network-error-alert.module';
-import { CivicTimeagoFormatter } from './core/utilities/timeago-formatter';
-import { CvcFormsModule } from './forms/forms.module';
-import { Observable } from 'rxjs';
-import { AppLoadErrorHandler } from './core/utilities/app-reload-handler';
+} from '@angular/common/http'
+import { NgDatePipesModule } from 'ngx-pipes'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { LetModule, PushModule } from '@ngrx/component'
+import { CookieService } from 'ngx-cookie-service'
+import { civicIcons } from '@app/icons-provider.module'
+import { TimeagoFormatter, TimeagoModule } from 'ngx-timeago'
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger'
+import { NgxJsonViewerModule } from 'ngx-json-viewer'
+import { NZ_I18N } from 'ng-zorro-antd/i18n'
+import { en_US } from 'ng-zorro-antd/i18n'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { GraphQLModule } from '@app/graphql/graphql.module'
+import { environment } from 'environments/environment'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { CvcNetworkErrorAlertModule } from './components/app/network-error-alert/network-error-alert.module'
+import { CivicTimeagoFormatter } from './core/utilities/timeago-formatter'
+import { CvcFormsModule } from './forms/forms.module'
+import { Observable } from 'rxjs'
+import { AppLoadErrorHandler } from './core/utilities/app-reload-handler'
 
-registerLocaleData(en);
+registerLocaleData(en)
 
 function initializeApiFactory(httpClient: HttpClient): () => Observable<any> {
-  return () => httpClient.get('/api/status');
+  return () => httpClient.get('/api/status')
 }
 
 @NgModule({
@@ -54,7 +55,9 @@ function initializeApiFactory(httpClient: HttpClient): () => Observable<any> {
     }),
     NgxJsonViewerModule,
     NzIconModule.forRoot(civicIcons),
-    LetModule, PushModule,
+    LetModule,
+    PushModule,
+    NgDatePipesModule,
     TimeagoModule.forRoot({
       formatter: { provide: TimeagoFormatter, useClass: CivicTimeagoFormatter },
     }),
