580245ff8f634aa545f66bcd728e90029e136d21
Adam Coffman <acoffman@wustl.edu>
2023-01-18 10:50:00 -0600

    remove any type on tooltip

--- client/src/app/core/pipes/evidence-enum-tooltip.pipe.ts
+++ client/src/app/core/pipes/evidence-enum-tooltip.pipe.ts
@@ -8,9 +8,7 @@ import { getEvidenceEnumTooltip } from '../utilities/enum-tooltips/get-evidence-
 })
 export class EvidenceEnumTooltipPipe implements PipeTransform {
   transform(
-    // NOTE: #UPDATE had to switch to any here, EvidenceType and other enums are not being interpreted as 'symbol' now.
-    // value?: string | symbol | number, // value of attribute
-    value?: any, // value of attribute
+    value?: string | symbol | number, // value of attribute
     name?: string | symbol, // name of entity attribute
     // optional contextual type (evidence/assertionType)
     // contextType: Maybe<symbol> = undefined,
