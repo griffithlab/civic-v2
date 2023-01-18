371bcb0810494d8f23eaaeddf7a409052acae9d8
Joshua McMichael <jmcmicha@wustl.edu>
2023-01-17 22:40:36 -0600

    updated autoHeightDiv directive with 'target' input

--- client/src/app/forms2/wrappers/form-layout/form-layout.wrapper.module.ts
+++ client/src/app/forms2/wrappers/form-layout/form-layout.wrapper.module.ts
@@ -1,22 +1,23 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { CvcOrgSelectorBtnGroupModule } from '@app/forms/config/components/org-selector-btn-group/org-selector-btn-group.module';
-import { CvcFormDebugComponentModule } from '@app/forms2/components/form-debug/form-debug.component.module';
-import { ConfigOption, FormlyModule } from '@ngx-formly/core';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzListModule } from 'ng-zorro-antd/list';
-import { NzPopoverModule } from 'ng-zorro-antd/popover';
-import { NzTabsModule } from 'ng-zorro-antd/tabs';
-import { NzTagModule } from 'ng-zorro-antd/tag';
-import { NgxJsonViewerModule } from 'ngx-json-viewer';
-import { CvcFormLayoutWrapper } from './form-layout.wrapper';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { CvcAutoHeightDivModule } from '@app/directives/auto-height-div/auto-height-div.module'
+import { CvcOrgSelectorBtnGroupModule } from '@app/forms/config/components/org-selector-btn-group/org-selector-btn-group.module'
+import { CvcFormDebugComponentModule } from '@app/forms2/components/form-debug/form-debug.component.module'
+import { ConfigOption, FormlyModule } from '@ngx-formly/core'
+import { NzButtonModule } from 'ng-zorro-antd/button'
+import { NzCardModule } from 'ng-zorro-antd/card'
+import { NzGridModule } from 'ng-zorro-antd/grid'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NzListModule } from 'ng-zorro-antd/list'
+import { NzPopoverModule } from 'ng-zorro-antd/popover'
+import { NzTabsModule } from 'ng-zorro-antd/tabs'
+import { NzTagModule } from 'ng-zorro-antd/tag'
+import { NgxJsonViewerModule } from 'ngx-json-viewer'
+import { CvcFormLayoutWrapper } from './form-layout.wrapper'
 
 const wrapperConfig: ConfigOption = {
   wrappers: [{ name: 'form-layout', component: CvcFormLayoutWrapper }],
-};
+}
 
 @NgModule({
   declarations: [CvcFormLayoutWrapper],
@@ -32,7 +33,7 @@ const wrapperConfig: ConfigOption = {
     NzPopoverModule,
     NzListModule,
     NzGridModule,
-
+    CvcAutoHeightDivModule,
     CvcOrgSelectorBtnGroupModule,
     CvcFormDebugComponentModule,
 
