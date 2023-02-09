2d3e027f18b12558b0b8b5a884e133434c3f37b2
Joshua McMichael <jmcmicha@wustl.edu>
2023-02-09 11:42:53 -0600

    mp-builder updates molecularProfileId on parent form

--- client/src/app/forms2/config/evidence-submit/evidence-submit.form.module.ts
+++ client/src/app/forms2/config/evidence-submit/evidence-submit.form.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
 import { CvcFormSubmissionStatusDisplayModule } from '@app/forms2/components/form-submission-status-display/form-submission-status-display.module'
 import { CvcForms2Module } from '@app/forms2/forms2.module'
 import { LetModule, PushModule } from '@ngrx/component'
@@ -11,6 +12,7 @@ import { CvcEvidenceSubmitForm } from './evidence-submit.form'
   declarations: [CvcEvidenceSubmitForm],
   imports: [
     CommonModule,
+    RouterModule,
     LetModule,
     PushModule,
     NzButtonModule,
