26a3f2c0ef5a1aa3d6bd3569d328f3f1160bf174
Joshua McMichael <jmcmicha@wustl.edu>
2023-02-15 13:04:13 -0600

    pending-revisions page imports working

--- client/src/app/views/curation/curation.module.ts
+++ client/src/app/views/curation/curation.module.ts
@@ -7,6 +7,7 @@ import { CurationQueuesModule } from './curation-queues/curation-queues.module';
 import { PendingSourcesModule } from './curation-queues/pending-sources/pending-sources.module';
 import { PendingEvidenceModule } from './curation-queues/pending-evidence/pending-evidence.module';
 import { PendingAssertionsModule } from './curation-queues/pending-assertions/pending-assertions.module';
+import { PendingRevisionsModule } from './curation-queues/pending-revisions/pending-revisions.module';
 
 @NgModule({
   declarations: [CurationView],
@@ -17,6 +18,7 @@ import { PendingAssertionsModule } from './curation-queues/pending-assertions/pe
     PendingSourcesModule,
     PendingEvidenceModule,
     PendingAssertionsModule,
+    PendingRevisionsModule,
   ]
 })
 export class CurationModule { }
