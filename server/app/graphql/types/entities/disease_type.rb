c88c33a83d2e5a155f8cd84eb3dc013e345ac0f8
Adam Coffman <acoffman@wustl.edu>
2023-02-22 11:23:47 -0600

    add initial implementation of MyDisease.info integration

--- server/app/graphql/types/entities/disease_type.rb
+++ server/app/graphql/types/entities/disease_type.rb
@@ -7,11 +7,16 @@ module Types::Entities
     field :disease_url, String, null: true
     field :disease_aliases, [String], null: false
     field :link, String, null: false
+    field :my_disease_info, Types::Entities::MyDiseaseInfoType, null: true
 
     def disease_aliases
       Loaders::AssociationLoader.for(Disease, :disease_aliases).load(object).then do |disease_aliases|
         disease_aliases.map{|d| d.name}.sort
       end
+    end
+
+    def my_disease_info
+      MyDiseaseInfo.new(object).response
     end
   end
 end
