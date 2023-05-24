113a381b17efa4734e48d5d340a2282fa5ea95fc
Adam Coffman <acoffman@wustl.edu>
2023-05-24 14:04:02 -0500

    add helper method instead of relationship

--- server/app/models/activity.rb
+++ server/app/models/activity.rb
@@ -5,12 +5,15 @@ class Activity < ApplicationRecord
 
 
   has_many :activity_linked_entities
-  has_many :linked_entities, through: :activity_linked_entities, source: :entity
   has_many :events
 
 
   def generate_verbiage
     raise NotImplementedError.new('Subclass must implement generate_verbiage for type')
+  end
+
+  def linked_entities
+    activity_linked_entities.includes(:entity).map(&:entity)
   end
 
   def link_entities!(entities)
