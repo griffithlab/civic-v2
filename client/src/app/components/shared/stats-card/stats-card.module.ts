a55d09f92b3e9d730c0aac2e2e19008b7d2299fe
Joshua McMichael <jmcmichael@users.noreply.github.com>
2023-09-14 10:42:52 -0500

    refactored activity counts & detailed stats components

--- client/src/app/components/shared/stats-card/stats-card.module.ts
+++ client/src/app/components/shared/stats-card/stats-card.module.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { CvcPipesModule } from '@app/core/pipes/pipes.module'
 import { LetDirective, PushPipe } from '@ngrx/component'
-import { NzCardModule } from 'ng-zorro-antd/card'
+import { NzBadgeModule } from 'ng-zorro-antd/badge'
+import { NzButtonModule } from 'ng-zorro-antd/button'
 import { NzGridModule } from 'ng-zorro-antd/grid'
 import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NzPopoverModule } from 'ng-zorro-antd/popover'
 import { NzStatisticModule } from 'ng-zorro-antd/statistic'
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip'
 import { NzTypographyModule } from 'ng-zorro-antd/typography'
 import { CvcStatsCardComponent } from './stats-card.component'
 
@@ -15,11 +18,14 @@ import { CvcStatsCardComponent } from './stats-card.component'
     CommonModule,
     LetDirective,
     PushPipe,
+    NzButtonModule,
     NzStatisticModule,
     NzTypographyModule,
     NzIconModule,
     NzGridModule,
-    NzCardModule,
+    NzBadgeModule,
+    NzToolTipModule,
+    NzPopoverModule,
     CvcPipesModule,
   ],
   exports: [CvcStatsCardComponent],
