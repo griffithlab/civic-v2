1174c5b3990b39c74775af18af3b4318eef03938
Adam Coffman <acoffman@wustl.edu>
2023-10-24 14:27:05 -0500

    Fix typo

--- server/app/scrapers/asco_query_response.rb
+++ server/app/scrapers/asco_query_response.rb
@@ -7,7 +7,7 @@ class AscoQueryResponse
   def citations
     json['response']['docs'].each_with_object([]) do |element, o|
       o.append({
-        citation: [author_for_element(element), element['Year']}"].join(', '),
+        citation: [author_for_element(element), element['Year']].join(', '),
         citation_id: element['_id'],
         source_type: 'ASCO',
         status: 'new',
