da24b01bc7aa164bd0733338dd99c9fce0af2779
Adam Coffman <acoffman@wustl.edu>
2023-10-24 13:48:02 -0500

    "final" backfill scripts

--- server/misc_scripts/activity_backfill/backfill_comment_activities.rb
+++ server/misc_scripts/activity_backfill/backfill_comment_activities.rb
@@ -1,16 +1,24 @@
 #NOTE: cannot run this until last
 Event.where(action: 'commented', activity_id: nil).find_each do |event|
-  activity = CommentActivity.create(
-    subject: event.subject,
-    user_id: event.originating_user_id,
-    organization_id: event.organization_id,
-    created_at: event.created_at
-  )
+  if event.originating_object.blank?
+    Notification.where(event: event).destroy_all
+    event.destroy!
+  else
+    activity = CommentActivity.create(
+      subject: event.subject,
+      user_id: event.originating_user_id,
+      organization_id: event.organization_id,
+      created_at: event.created_at
+    )
 
-  event.activity_id = activity.id
-  event.save!
+    event.activity_id = activity.id
+    event.save!
 
-  activity.link_entities!(event.originating_object)
-  activity.verbiage = activity.generate_verbiage
-  activity.save!
+    activity.link_entities!(event.originating_object)
+    activity.verbiage = activity.generate_verbiage
+    activity.save!
+  end
 end
+
+
+
---
+++ server/misc_scripts/activity_backfill/backfill_notes.rb
@@ -0,0 +1,24 @@
+Activity.where.not(type: ["CommentActivity", 'ModerateAssertionActivity', 'ModerateEvidenceItemActivity'])
+.find_each do |a|
+  events = Event.where(action: 'commented', activity_id: a.id)
+  events.each do |e|
+    comment = e&.originating_object
+    if comment
+      a.note = comment.comment
+      a.save!
+      Notification.where(event: e).destroy_all
+      v = Variant.where(deprecation_comment_id: comment.id)
+      v.each do |variant|
+        variant.deprecation_comment_id = nil
+        variant.save!(validate: false)
+      end
+      EntityMention.where(comment: comment).destroy_all
+      UserMention.where(comment: comment).destroy_all
+      ActivityLinkedEntity.where(entity: comment).destroy_all
+      comment.destroy!
+      e.destroy!
+    elsif e.present? && comment.nil?
+      binding.pry
+    end
+  end
+end
