80d816d59446aeff443acd53239783dbd49ce82a
Joshua McMichael <jmcmichael@users.noreply.github.com>
2024-01-16 16:36:23 -0600

    replaced old activity-feed component

--- client/src/app/views/curation/curation-activity/curation-timeline/curation-timeline.module.ts
+++ client/src/app/views/curation/curation-activity/curation-timeline/curation-timeline.module.ts
@@ -4,16 +4,9 @@ import { NzGridModule } from 'ng-zorro-antd/grid'
 import { NzCardModule } from 'ng-zorro-antd/card'
 import { CvcEventFeedModule } from '@app/components/events/event-feed/event-feed.module'
 import { CurationTimelinePage } from './curation-timeline.page'
-import { CvcActivityFeedModule } from '@app/components/activities/activity-feed/activity-feed.module'
 
 @NgModule({
   declarations: [CurationTimelinePage],
-  imports: [
-    CommonModule,
-    NzGridModule,
-    NzCardModule,
-    CvcEventFeedModule,
-    CvcActivityFeedModule
-  ],
+  imports: [CommonModule, NzGridModule, NzCardModule, CvcEventFeedModule],
 })
 export class CurationTimelineModule {}
