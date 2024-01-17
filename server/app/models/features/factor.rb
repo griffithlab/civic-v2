bfa84c14c5a4c7627104dc5fe241eceeb1ab5a06
Adam Coffman <acc@fastmail.com>
2024-01-17 15:18:09 -0600

    stub in factor classes

---
+++ server/app/models/features/factor.rb
@@ -0,0 +1,19 @@
+module Features
+  class Factor < ActiveRecord::Base
+    include Subscribable
+    include IsFeatureInstance
+
+    has_many :comment_mentions, foreign_key: :comment_id, class_name: 'EntityMention'
+
+    def display_name
+      name
+    end
+
+    def editable_fields
+      [
+        :description,
+        :source_ids
+      ]
+    end
+  end
+end
