4ecb71f0b46cf07f4a42b980f8edaba3f25ebdca
Joshua McMichael <jmcmichael@users.noreply.github.com>
2024-01-25 09:15:36 -0600

    componentized feed-settings

--- client/src/app/components/activities/activity-feed-settings/activity-feed-settings.component.ts
+++ client/src/app/components/activities/activity-feed-settings/activity-feed-settings.component.ts
@@ -1,9 +1,23 @@
 import { CommonModule } from '@angular/common'
-import { Component, WritableSignal, signal } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  WritableSignal,
+  signal,
+} from '@angular/core'
 import { FormsModule } from '@angular/forms'
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox'
+import { NzFormModule } from 'ng-zorro-antd/form'
 import { NzGridModule } from 'ng-zorro-antd/grid'
+import { NzIconModule } from 'ng-zorro-antd/icon'
 import { NzPopoverModule } from 'ng-zorro-antd/popover'
 import { NzSelectModule } from 'ng-zorro-antd/select'
+
+export type CvcActivityFeedSettingsOptions = {
+  pageSizes: number[]
+  includeAutomatedEvents: boolean
+}
 
 @Component({
   selector: 'cvc-activity-feed-settings',
@@ -11,19 +25,30 @@ import { NzSelectModule } from 'ng-zorro-antd/select'
   imports: [
     CommonModule,
     FormsModule,
+    NzCheckboxModule,
+    NzIconModule,
     NzGridModule,
     NzPopoverModule,
     NzSelectModule,
   ],
   templateUrl: './activity-feed-settings.component.html',
   styleUrl: './activity-feed-settings.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CvcActivityFeedSettings {
-  includeAutomatedEvents: WritableSignal<boolean>
+  @Input() cvcFeedSettingsOptions: CvcActivityFeedSettingsOptions
+
+  optionsDefaults: CvcActivityFeedSettingsOptions = {
+    pageSizes: [5, 10, 25, 50, 100],
+    includeAutomatedEvents: false,
+  }
+
+  includeAutomatedEvents: WritableSignal<boolean> = signal(false)
   pageSize: WritableSignal<number>
   pageSizes = [5, 10, 25, 50, 100]
 
   constructor() {
+    this.cvcFeedSettingsOptions = this.optionsDefaults
     this.includeAutomatedEvents = signal(false)
     this.pageSize = signal(10)
   }
