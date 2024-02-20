d66f43c64b2bf7b044e5293b9a77bcbc88894d1d
Joshua McMichael <jmcmichael@users.noreply.github.com>
2024-02-20 09:16:45 -0600

    upgraded apollo-angular, dependencies

--- client/src/app/generated/civic.possible-types.ts
+++ client/src/app/generated/civic.possible-types.ts
@@ -1,9 +1,101 @@
 
-      export interface PossibleTypesResultData {
-        possibleTypes: {
-          [key: string]: string[]
-        }
-      }
+      export type PossibleTypesResultData = {
+  "possibleTypes": {
+    "ActivityInterface": [
+      "AcceptRevisionsActivity",
+      "CommentActivity",
+      "CreateComplexMolecularProfileActivity",
+      "CreateVariantActivity",
+      "DeprecateComplexMolecularProfileActivity",
+      "DeprecateVariantActivity",
+      "FlagEntityActivity",
+      "ModerateAssertionActivity",
+      "ModerateEvidenceItemActivity",
+      "RejectRevisionsActivity",
+      "ResolveFlagActivity",
+      "SubmitAssertionActivity",
+      "SubmitEvidenceItemActivity",
+      "SuggestRevisionSetActivity",
+      "SuggestSourceActivity",
+      "UpdateSourceSuggestionStatusActivity"
+    ],
+    "CommentBodySegment": [
+      "CommentTagSegment",
+      "CommentTextSegment",
+      "User"
+    ],
+    "Commentable": [
+      "Assertion",
+      "EvidenceItem",
+      "Flag",
+      "Gene",
+      "MolecularProfile",
+      "Revision",
+      "Source",
+      "SourcePopover",
+      "Variant",
+      "VariantGroup"
+    ],
+    "EventOriginObject": [
+      "Assertion",
+      "Comment",
+      "EvidenceItem",
+      "Flag",
+      "MolecularProfile",
+      "Revision",
+      "SourceSuggestion",
+      "Variant"
+    ],
+    "EventSubject": [
+      "Assertion",
+      "EvidenceItem",
+      "Flag",
+      "Gene",
+      "MolecularProfile",
+      "Revision",
+      "RevisionSet",
+      "Source",
+      "SourcePopover",
+      "SourceSuggestion",
+      "Variant",
+      "VariantGroup"
+    ],
+    "Flaggable": [
+      "Assertion",
+      "BrowseGene",
+      "EvidenceItem",
+      "Gene",
+      "MolecularProfile",
+      "Variant",
+      "VariantGroup"
+    ],
+    "ModeratedField": [
+      "ObjectField",
+      "ScalarField"
+    ],
+    "ModeratedFieldDiff": [
+      "ObjectFieldDiff",
+      "ScalarFieldDiff"
+    ],
+    "MolecularProfileComponent": [
+      "Gene",
+      "Variant"
+    ],
+    "MolecularProfileSegment": [
+      "Gene",
+      "MolecularProfileTextSegment",
+      "Variant"
+    ],
+    "WithRevisions": [
+      "Assertion",
+      "EvidenceItem",
+      "Gene",
+      "MolecularProfile",
+      "Variant",
+      "VariantGroup"
+    ]
+  }
+};
       const result: PossibleTypesResultData = {
   "possibleTypes": {
     "ActivityInterface": [
