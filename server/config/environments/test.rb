eaab144f47ade75708b0a5eb6e5d67e7d9012dfd
Adam Coffman <acc@fastmail.com>
2024-04-23 11:05:12 -0500

    eager load files in CI env

--- server/config/environments/test.rb
+++ server/config/environments/test.rb
@@ -11,10 +11,8 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
-  # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  # Eager load when running in CI to detect any load issues
+  config.eager_load = ENV["CI"].present?
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
