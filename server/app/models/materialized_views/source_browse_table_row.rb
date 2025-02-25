68165035382a36c103a046042e5bee83b9a0edae
Adam Coffman <acc@fastmail.com>
2025-02-24 19:34:43 -0600

    add another missed enum

--- server/app/models/materialized_views/source_browse_table_row.rb
+++ server/app/models/materialized_views/source_browse_table_row.rb
@@ -2,5 +2,5 @@ class MaterializedViews::SourceBrowseTableRow < MaterializedViews::MaterializedV
   self.primary_key = :id
   has_and_belongs_to_many :clinical_trials, join_table: :clinical_trials_sources, foreign_key: :source_id
 
-  enum source_type: ['PubMed', 'ASCO', 'ASH']
+  enum :source_type, ['PubMed', 'ASCO', 'ASH']
 end
