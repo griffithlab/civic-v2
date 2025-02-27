b3cde7d6eda4741ef1966a74965af3aa1d3f2a66
Adam Coffman <acc@fastmail.com>
2025-02-27 10:54:28 -0600

    dont prepend reg_ to graphql enum

--- server/app/graphql/types/fusion/regulatory_fusion_type_type.rb
+++ server/app/graphql/types/fusion/regulatory_fusion_type_type.rb
@@ -2,7 +2,7 @@ module Types::Fusion
   class RegulatoryFusionTypeType < Types::BaseEnum
     Constants::REGULATORY_FUSION_ENUM_TYPES
       .each do |(name, _)|
-        value Features::Fusion.format_regulatory_fusion_type(name).upcase, value: name
+        value name, value: name
     end
   end
 end
