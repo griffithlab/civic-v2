848c4919d4df1a60ed2d2f91e0dd0525c1a17dd4
Susanna Kiwala <susanna.kiwala@wustl.edu>
2025-04-03 11:55:42 -0500

    Add revocation_activity and endorsement_activity shortcuts on endorsements

--- server/app/models/endorsement.rb
+++ server/app/models/endorsement.rb
@@ -9,5 +9,17 @@ class Endorsement < ApplicationRecord
     class_name: "ActivityLinkedEntity"
   has_many :activities, through: :activities_linked_entities
 
+  has_one :revocation_activity_link,
+    ->() { where(entity_type: "Endorsement").eager_load(:activity).where("activities.type = 'RevokeEndorsementActivity'").order("activities.created_at desc") },
+    foreign_key: :entity_id,
+    class_name: "ActivityLinkedEntity"
+  has_one :revocation_activity, through: :revocation_activity_link, source: :activity
+
+  has_one :endorsement_activity_link,
+    ->() { where(entity_type: "Endorsement").eager_load(:activity).where("activities.type = 'EndorseAssertionActivity'").order("activities.created_at desc") },
+    foreign_key: :entity_id,
+    class_name: "ActivityLinkedEntity"
+  has_one :endorsement_activity, through: :endorsement_activity_link, source: :activity
+
   enum :status, Constants::ENDORSEMENT_STATUSES
 end
