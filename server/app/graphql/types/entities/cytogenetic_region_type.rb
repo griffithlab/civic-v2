77482f4e00c952ccd8e3294d7f47269f0289d20a
Susanna Kiwala <susanna.kiwala@wustl.edu>
2025-04-03 09:37:01 -0500

    Add new graphql types

---
+++ server/app/graphql/types/entities/cytogenetic_region_type.rb
@@ -0,0 +1,13 @@
+module Types::Entities
+  class CytogeneticRegionType < Types::BaseObject
+    field :id, Int, null: false
+    field :name, String, null: false
+    field :chromosome, String, null: false
+    field :band, String, null: true
+    field :cytogenetic_coordinates, Types::Entities::CytogeneticCoordinateType, null: false
+
+    def cytogenetic_coordinates
+      Loaders::AssociationLoader.for(CytogeneticRegion, :cytogenetic_coordinates).load(object)
+    end
+  end
+end
