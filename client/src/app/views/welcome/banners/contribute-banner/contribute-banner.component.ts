ad1b2983be7e0199b924ab088d21228b0c8210cc
Joshua McMichael <jmcmichael@users.noreply.github.com>
2025-06-18 11:40:32 -0500

    styled welcome banners

--- client/src/app/views/welcome/banners/contribute-banner/contribute-banner.component.ts
+++ client/src/app/views/welcome/banners/contribute-banner/contribute-banner.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'cvc-contribute-banner',
   templateUrl: 'contribute-banner.component.html',
   styleUrls: ['contribute-banner.component.less'],
+  imports: [RouterLink],
 })
 export class CvcContributeBannerComponent {}
--- client/src/app/views/welcome/banners/discover-banner/discover-banner.component.ts
+++ client/src/app/views/welcome/banners/discover-banner/discover-banner.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'cvc-discover-banner',
   templateUrl: 'discover-banner.component.html',
   styleUrls: ['discover-banner.component.less'],
+  imports: [RouterLink],
 })
 export class CvcDiscoverBannerComponent {}
