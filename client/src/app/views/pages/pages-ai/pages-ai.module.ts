be5b59dc329bdf617cbdc6e0a0c7c1c76cbcc61a
Joshua McMichael <jmcmichael@fastmail.com>
2025-10-07 12:01:45 -0500

    basic ai-tools page structure

--- client/src/app/views/pages/pages-ai/pages-ai.module.ts
+++ client/src/app/views/pages/pages-ai/pages-ai.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common'
 import { PagesAiPage } from './pages-ai.page'
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header'
 import { NzGridModule } from 'ng-zorro-antd/grid'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NzTypographyModule } from 'ng-zorro-antd/typography'
+import { NzCardModule } from 'ng-zorro-antd/card'
 
 @NgModule({
   declarations: [PagesAiPage],
-  imports: [CommonModule, NzPageHeaderModule, NzGridModule],
+  imports: [
+    CommonModule,
+    NzPageHeaderModule,
+    NzGridModule,
+    NzIconModule,
+    NzCardModule,
+    NzTypographyModule,
+  ],
 })
 export class PagesAiModule {}
