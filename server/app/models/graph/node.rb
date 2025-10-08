91fd7b1d989f97df6f1929a9066172aaa6e13aeb
Adam Coffman <acc@fastmail.com>
2025-10-08 10:37:08 -0500

    remove redundant destroy config

--- server/app/models/graph/node.rb
+++ server/app/models/graph/node.rb
@@ -3,9 +3,9 @@ module Graph
     belongs_to :term, polymorphic: true
 
     has_many :outgoing_edges, class_name: "Graph::Edge", foreign_key: :previous_node_id, dependent: :destroy
-    has_many :next_nodes, through: :outgoing_edges, source: :next_node, dependent: :destroy
+    has_many :next_nodes, through: :outgoing_edges, source: :next_node
 
     has_many :incoming_edges, class_name: "Graph::Edge", foreign_key: :next_node_id, dependent: :destroy
-    has_many :previous_nodes, through: :incoming_edges, source: :previous_node, dependent: :destroy
+    has_many :previous_nodes, through: :incoming_edges, source: :previous_node
   end
 end
