0d696f247893c967bda5b4e497b86956477f3894
Adam Coffman <acc@fastmail.com>
2025-10-23 10:23:56 -0500

    import therapy graph

--- server/app/lib/importer/nci_thesaurus_mirror.rb
+++ server/app/lib/importer/nci_thesaurus_mirror.rb
@@ -3,7 +3,7 @@ require "zip"
 
 module Importer
   class NciThesaurusMirror
-    attr_reader :parser, :version
+    attr_reader :parser, :version, :parents
 
     def initialize(path, version = Time.now.utc.iso8601)
       zip_file = Zip::File.open(path)
@@ -16,18 +16,23 @@ module Importer
         headers: [ "code", "concept_iri", "parents", "synonyms", "definition", "display_name", "concept_status", "semantic_type", "concept_in_subset" ],
       )
       @version = version
+      @parents = {}
     end
 
     def import
       parser.each do |elem|
         if valid_entry?(elem)
           create_object_from_entry(elem)
+          store_parent(elem)
         end
       end
+      create_graph
     end
 
     def valid_entry?(entry)
-      valid_semantic_types.include?(entry["semantic_type"]) && entry["concept_status"].nil?
+      entry["concept_status"].nil? && entry["semantic_type"].split("|").any? do |t|
+        valid_semantic_types.include?(t)
+      end
     end
 
     def valid_semantic_types
@@ -47,6 +52,28 @@ module Importer
         end
       end
       therapy.save
+    end
+
+    def store_parent(elem)
+      if elem["parents"].present?
+        parents[elem["code"]] = elem["parents"]
+          .split("|")
+          .map(&:strip)
+      end
+    end
+
+    def create_graph
+      parents.each do |elem_ncit, parent_ncits|
+        parent_ncits.each do |parent_ncit|
+          parent = Therapy.find_by(ncit_id: parent_ncit)
+          child = Therapy.find_by(ncit_id: elem_ncit)
+          if parent.present? && child.present?
+            parent.add_child_term(child, relationship: "is_a")
+          else
+            Rails.logger.warn("Unexpected unknown NCIt: #{elem_ncit} or #{parent_ncit}")
+          end
+        end
+      end
     end
   end
 end
