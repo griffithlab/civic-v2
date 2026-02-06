9b040d73b5b114c29329932ddc817d23253ef600
Adam Coffman <acc@fastmail.com>
2026-02-06 15:28:49 -0600

    use redis cache for tracking IP based rate limiting

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   include Authentication
   include ActiveStorage::SetCurrent
+
+  RATE_LIMIT_STORE = ActiveSupport::Cache::RedisCacheStore.new
+
+  # sustained 2 req/s
+  rate_limit to: 600, within: 5.minutes,
+    store: RATE_LIMIT_STORE,
+    scope: :api_requests,
+    with: -> do
+      if !signed_in?
+        head 429
+      end
+    end
 end
