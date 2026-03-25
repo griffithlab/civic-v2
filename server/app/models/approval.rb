d6279e04b549166b088966950d6165ba1eff928f
Adam Coffman <acc@fastmail.com>
2026-03-25 10:01:47 -0500

    fix typo

--- server/app/models/approval.rb
+++ server/app/models/approval.rb
@@ -35,7 +35,7 @@ class Approval < ApplicationRecord
       return true
     elsif existing_batch_entries.any? { |be| be.approval.last_reviewed == self.last_reviewed && be.status != "error" }
       # we have a batch entry that corresponds to this approval,
-      # and it already was succesfully submitted or is pending
+      # and it already was successfully submitted or is pending
       return false
     elsif existing_batch_entries.all? { |be| be.approval_last_reviewed < self.last_reviewed }
       # previous submissions exist (error or success) but the approval has been re-reviewed since then
