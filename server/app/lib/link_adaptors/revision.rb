9cb8ba9a0a4c7d3fe4d8e4293a2dc3a00cae4a06
Susanna Kiwala <susanna.kiwala@wustl.edu>
2026-08-19 14:32:11 -0500

    Handle /links/revisions/:id shortcut for revisions on variants

--- server/app/lib/link_adaptors/revision.rb
+++ server/app/lib/link_adaptors/revision.rb
@@ -6,7 +6,12 @@ module LinkAdaptors
 
     def base_path
       subject = obj.subject
-      subject_adaptor = "LinkAdaptors::#{subject.class}".constantize.new(subject)
+      adaptor = if [ "Variants::GeneVariant", "Variants::FactorVariant", "Variants::FusionVariant", "Variants::RegionVariant" ].include? (subject.class.to_s)
+        "Variant"
+      else
+        subject.class
+      end
+      subject_adaptor = "LinkAdaptors::#{adaptor}".constantize.new(subject)
       subject_adaptor.base_path + "/revisions"
     end
   end
